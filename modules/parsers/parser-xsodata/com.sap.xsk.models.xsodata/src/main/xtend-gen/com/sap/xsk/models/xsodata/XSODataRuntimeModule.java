51d14162594a9f6303fccaf099bc1bc6ecc4491e
delchev <delchevn@gmail.com>
2020-11-13 16:34:10 +0200

    left-overs

--- modules/parsers/parser-xsodata/com.sap.xsk.models.xsodata/src/main/xtend-gen/com/sap/xsk/models/xsodata/XSODataRuntimeModule.java
+++ modules/parsers/parser-xsodata/com.sap.xsk.models.xsodata/src/main/xtend-gen/com/sap/xsk/models/xsodata/XSODataRuntimeModule.java
@@ -3,9 +3,11 @@
  */
 package com.sap.xsk.models.xsodata;
 
+import com.sap.xsk.models.xsodata.AbstractXSODataRuntimeModule;
+
 /**
  * Use this class to register components to be used at runtime / without the Equinox extension registry.
  */
 @SuppressWarnings("all")
-public class XSODataRuntimeModule /* implements AbstractXSODataRuntimeModule  */{
+public class XSODataRuntimeModule extends AbstractXSODataRuntimeModule {
 }

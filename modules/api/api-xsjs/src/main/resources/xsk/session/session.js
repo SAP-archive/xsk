8e58850e9608b2c54591cfcc426aa7cd199f6b4d
Boris Nenchovski <26414287+BorisNen@users.noreply.github.com>
2021-05-21 17:39:27 +0300

    Added getTimeout method to the session API
    Signed-off-by: Boris Nenchovski <26414287+BorisNen@users.noreply.github.com>

--- modules/api/api-xsjs/src/main/resources/xsk/session/session.js
+++ modules/api/api-xsjs/src/main/resources/xsk/session/session.js
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 2019-2020 SAP SE or an SAP affiliate company and XSK contributors
+ * Copyright (c) 2019-2021 SAP SE or an SAP affiliate company and XSK contributors
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Apache License, v2.0
  * which accompanies this distribution, and is available at
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * SPDX-FileCopyrightText: 2019-2020 SAP SE or an SAP affiliate company and XSK contributors
+ * SPDX-FileCopyrightText: 2019-2021 SAP SE or an SAP affiliate company and XSK contributors
  * SPDX-License-Identifier: Apache-2.0
  */
 var securityUser = require('security/v4/user');
@@ -35,6 +35,10 @@ exports.assertAppPrivilege = function (privilegeName) {
     if (!hasPrivilege) {
         throw new NoSuchPrivilegeException(privilegeName);
     }
+}
+
+exports.getTimeout = function () {
+  return securityUser.getTimeout()
 }
 
 function NoSuchPrivilegeException(privilegeToCheck) {

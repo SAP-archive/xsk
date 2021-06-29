570204b1cb5d65b93612d13c1af3c500f03debcd
StanZGenchev <stanislav.genchev@sap.com>
2021-06-29 08:45:21 +0300

    Updated dirigible and migration UI versions
    Signed-off-by: StanZGenchev <stanislav.genchev@sap.com>

--- modules/ide/ide-migration/src/main/resources/ide-migration/js/start-migration.js
+++ modules/ide/ide-migration/src/main/resources/ide-migration/js/start-migration.js
@@ -9,23 +9,74 @@
  * SPDX-FileCopyrightText: 2019-2021 SAP SE or an SAP affiliate company and XSK contributors
  * SPDX-License-Identifier: Apache-2.0
  */
-migrationLaunchView.controller('StartMigrationViewController', ['$scope', '$messageHub', function ($scope, $messageHub) {
+migrationLaunchView.controller('StartMigrationViewController', ['$scope', '$http', '$messageHub', function ($scope, $http, $messageHub) {
     $scope.isVisible = false;
     $scope.migrationFinished = false;
-    $scope.progressBarPercentage = 10;
+    $scope.progressBarPercentage = 100;
     let titleList = [
         "Configuration processing, starting the migration...",
         "Migration complete"
     ]
     $scope.progressTitle = titleList[0];
+    let neoData = undefined;
+    let hanaData = undefined;
+    let defaultErrorTitle = "Error migrating project";
+    let defaultErrorDesc = "Please check if the information you provided is correct and try again.";
+
+    function startMigration(duData) {
+        body = {
+            neo: neoData,
+            hana: hanaData,
+            "connectionId": duData.connectionId,
+            "workspace": duData.workspace,
+            "vendor": duData.du.vendor,
+            "du": duData.du.name,
+        }
+        $http.post(
+            "/public/v4/migration-operations/execute-migration",
+            JSON.stringify(body),
+            { headers: { 'Content-Type': 'application/json' } }
+        ).then(function (response) {
+            $scope.migrationFinished = true;
+            $scope.progressTitle = titleList[1];
+        }, function (response) {
+            if (response.data) {
+                if ("error" in response.data) {
+                    if ("message" in response.data.error) {
+                        $messageHub.announceAlertError(
+                            defaultErrorTitle,
+                            response.data.error.message
+                        );
+                    } else {
+                        $messageHub.announceAlertError(
+                            defaultErrorTitle,
+                            defaultErrorDesc
+                        );
+                    }
+                    console.error(`HTTP $response.status`, response.data.error);
+                } else {
+                    $messageHub.announceAlertError(
+                        defaultErrorTitle,
+                        defaultErrorDesc
+                    );
+                }
+            } else {
+                $messageHub.announceAlertError(
+                    defaultErrorTitle,
+                    defaultErrorDesc
+                );
+            }
+            errorOccurred();
+        });
+    };
+
+    function errorOccurred() {
+        $scope.$parent.previousClicked();
+    }
 
     $scope.migrationDone = function () {
         $scope.migrationFinished = true;
     };
-
-    function sleep(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms));
-    }
 
     $messageHub.on('migration.start-migration', function (msg) {
         if ("isVisible" in msg.data) {
@@ -33,18 +84,19 @@ migrationLaunchView.controller('StartMigrationViewController', ['$scope', '$mess
                 $scope.isVisible = msg.data.isVisible;
             });
             if (msg.data.isVisible) {
-                sleep(1000).then(() => { $scope.$apply(function () { $scope.progressBarPercentage = 25; }); });
-                sleep(2000).then(() => { $scope.$apply(function () { $scope.progressBarPercentage = 40; }); });
-                sleep(3000).then(() => { $scope.$apply(function () { $scope.progressBarPercentage = 75; }); });
-                sleep(4000).then(() => { $scope.$apply(function () { $scope.progressBarPercentage = 90; }); });
-                sleep(6000).then(() => { $scope.$apply(function () { $scope.progressBarPercentage = 100; }); });
-                sleep(7000).then(() => {
-                    $scope.$apply(function () {
-                        $scope.migrationFinished = true;
-                        $scope.progressTitle = titleList[1];
-                    });
-                });
+                $messageHub.message('migration.neo-credentials', { controller: "migration.start-migration", getData: "all" });
+                $messageHub.message('migration.hana-credentials', { controller: "migration.start-migration", getData: "all" });
+                $messageHub.message('migration.delivery-unit', { controller: "migration.start-migration", getData: "all" });
             }
+        }
+        if ("neoData" in msg.data) {
+            neoData = msg.data.neoData;
+        }
+        if ("hanaData" in msg.data) {
+            hanaData = msg.data.hanaData;
+        }
+        if ("duData" in msg.data) {
+            startMigration(msg.data.duData);
         }
     }.bind(this));
 }]);

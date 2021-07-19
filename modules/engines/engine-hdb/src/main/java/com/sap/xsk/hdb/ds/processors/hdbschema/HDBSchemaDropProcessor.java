f5d821cfaaa16bf43df1b86f974d226d84a5cef5
antoanetamh <antoanetamh@gmail.com>
2021-07-19 12:03:49 +0300

    refactoring

--- modules/engines/engine-hdb/src/main/java/com/sap/xsk/hdb/ds/processors/hdbschema/HDBSchemaDropProcessor.java
+++ modules/engines/engine-hdb/src/main/java/com/sap/xsk/hdb/ds/processors/hdbschema/HDBSchemaDropProcessor.java
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 2019-2021 SAP SE or an SAP affiliate company and XSK contributors
+ * Copyright (c) 2021 SAP SE or an SAP affiliate company and XSK contributors
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Apache License, v2.0
  * which accompanies this distribution, and is available at
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * SPDX-FileCopyrightText: 2019-2021 SAP SE or an SAP affiliate company and XSK contributors
+ * SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and XSK contributors
  * SPDX-License-Identifier: Apache-2.0
  */
 package com.sap.xsk.hdb.ds.processors.hdbschema;
@@ -24,22 +24,26 @@ import org.slf4j.LoggerFactory;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import static java.text.MessageFormat.format;
+
 public class HDBSchemaDropProcessor extends AbstractXSKProcessor<XSKDataStructureHDBSchemaModel> {
 
-  private static final Logger logger = LoggerFactory.getLogger(HDBSchemaDropProcessor.class);
+    private static final Logger logger = LoggerFactory.getLogger(HDBSchemaDropProcessor.class);
 
-  public void execute(Connection connection, XSKDataStructureHDBSchemaModel hdbSchema) throws SQLException {
-    logger.info("Processing Drop Schema: " + hdbSchema.getSchema());
+    public void execute(Connection connection, XSKDataStructureHDBSchemaModel hdbSchema) throws SQLException {
+        logger.info("Processing Drop Schema: " + hdbSchema.getSchema());
 
-    ISqlDialect dialect = SqlFactory.deriveDialect(connection);
-    if (!(dialect.getClass().equals(HanaSqlDialect.class))) {
-      throw new IllegalStateException(String.format("%s does not support Schema", dialect.getDatabaseName(connection)));
-    } else {
+        ISqlDialect dialect = SqlFactory.deriveDialect(connection);
+        if (!(dialect.getClass().equals(HanaSqlDialect.class))) {
+            throw new IllegalStateException(String.format("%s does not support Schema", dialect.getDatabaseName(connection)));
+        } else {
             if (SqlFactory.getNative(connection).exists(connection, hdbSchema.getSchema(), DatabaseArtifactTypes.SCHEMA)) {
-      String schemaName = XSKHDBUtils.escapeArtifactName(connection, hdbSchema.getSchema());
+                String schemaName = XSKHDBUtils.escapeArtifactName(connection, hdbSchema.getSchema());
                 String sql = SqlFactory.getNative(connection).drop().schema(schemaName).build();
-      executeSql(sql, connection);
+                executeSql(sql, connection);
+            } else {
+                logger.warn(format("Schema [{0}] does not exists during the drop process", hdbSchema.getSchema()));
+            }
+        }
     }
-  }
-}
 }
--- modules/engines/engine-hdb/src/main/java/com/sap/xsk/hdb/ds/processors/synonym/HDBSynonymDropProcessor.java
+++ modules/engines/engine-hdb/src/main/java/com/sap/xsk/hdb/ds/processors/synonym/HDBSynonymDropProcessor.java
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 2019-2021 SAP SE or an SAP affiliate company and XSK contributors
+ * Copyright (c) 2021 SAP SE or an SAP affiliate company and XSK contributors
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Apache License, v2.0
  * which accompanies this distribution, and is available at
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * SPDX-FileCopyrightText: 2019-2021 SAP SE or an SAP affiliate company and XSK contributors
+ * SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and XSK contributors
  * SPDX-License-Identifier: Apache-2.0
  */
 package com.sap.xsk.hdb.ds.processors.synonym;
@@ -28,37 +28,37 @@ import static java.text.MessageFormat.format;
 
 public class HDBSynonymDropProcessor extends AbstractXSKProcessor<XSKDataStructureHDBSynonymModel> {
 
-  private static final Logger logger = LoggerFactory.getLogger(HDBSynonymDropProcessor.class);
+    private static final Logger logger = LoggerFactory.getLogger(HDBSynonymDropProcessor.class);
 
-  /**
-   * Execute :
-   * DROP SYNONYM <synonym_name> [<drop_option>]
-   * If <drop_option> is not specified, then a non-cascaded drop is performed which only drops the specified synonym.
-   * Dependent objects of the synonym are invalidated but not dropped.
-   *
-   * @see <a href="https://help.sap.com/viewer/4fe29514fd584807ac9f2a04f6754767/1.0.12/en-US/20d7e172751910148bccb49de92d9859.html">DROP SYNONYM Statement (Data Definition)</a>
-   */
-  @Override
-  public void execute(Connection connection, XSKDataStructureHDBSynonymModel synonymModel) throws SQLException {
-    synonymModel.getSynonymDefinitions().forEach((key, value) -> {
-      logger.info("Processing Drop Synonym: " + key);
+    /**
+     * Execute :
+     * DROP SYNONYM <synonym_name> [<drop_option>]
+     * If <drop_option> is not specified, then a non-cascaded drop is performed which only drops the specified synonym.
+     * Dependent objects of the synonym are invalidated but not dropped.
+     *
+     * @see <a href="https://help.sap.com/viewer/4fe29514fd584807ac9f2a04f6754767/1.0.12/en-US/20d7e172751910148bccb49de92d9859.html">DROP SYNONYM Statement (Data Definition)</a>
+     */
+    @Override
+    public void execute(Connection connection, XSKDataStructureHDBSynonymModel synonymModel) throws SQLException {
+        synonymModel.getSynonymDefinitions().forEach((key, value) -> {
+            logger.info("Processing Drop Synonym: " + key);
 
             String synonymName = (value.getSynonymSchema() != null) ? (XSKHDBUtils.escapeArtifactName(connection, key, value.getSynonymSchema())) : (XSKHDBUtils.escapeArtifactName(connection, key));
-      try {
+            try {
                 if (SqlFactory.getNative(connection).exists(connection, value.getSynonymSchema(), key, DatabaseArtifactTypes.SYNONYM)) {
-          ISqlDialect dialect = SqlFactory.deriveDialect(connection);
-          if (!(dialect.getClass().equals(HanaSqlDialect.class))) {
-            throw new IllegalStateException(String.format("Synonyms are not supported for %s !", dialect.getDatabaseName(connection)));
-          } else {
-            String sql = SqlFactory.getNative(connection).drop().synonym(synonymName).build();
-            executeSql(sql, connection);
-          }
-        } else {
+                    ISqlDialect dialect = SqlFactory.deriveDialect(connection);
+                    if (!(dialect.getClass().equals(HanaSqlDialect.class))) {
+                        throw new IllegalStateException(String.format("Synonyms are not supported for %s !", dialect.getDatabaseName(connection)));
+                    } else {
+                        String sql = SqlFactory.getNative(connection).drop().synonym(synonymName).build();
+                        executeSql(sql, connection);
+                    }
+                } else {
                     logger.warn(format("Synonym [{0}] does not exists during the drop process", value.getSynonymSchema() + "." + key));
-        }
-      } catch (SQLException exception) {
-        exception.printStackTrace();
-      }
-    });
-  }
+                }
+            } catch (SQLException exception) {
+                exception.printStackTrace();
+            }
+        });
+    }
 }

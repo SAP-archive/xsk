65a83ebe5af19c338dc07be475dbabbf312f1635
antoanetamh <antoanetamh@gmail.com>
2021-07-19 11:10:02 +0300

    add schema Itest

--- modules/engines/engine-hdb/src/itest/java/com/sap/xsk/hdb/ds/itest/hdbschema/XSKHDBSchemaParserHanaITTest.java
+++ modules/engines/engine-hdb/src/itest/java/com/sap/xsk/hdb/ds/itest/hdbschema/XSKHDBSchemaParserHanaITTest.java
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2021 SAP SE or an SAP affiliate company and XSK contributors
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License, v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and XSK contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
 package com.sap.xsk.hdb.ds.itest.hdbschema;/*
  * Copyright (c) 2019-2021 SAP SE or an SAP affiliate company and XSK contributors
  *
@@ -46,6 +57,7 @@ import java.sql.Statement;
 import javax.sql.DataSource;
 import org.eclipse.dirigible.commons.config.Configuration;
 import org.eclipse.dirigible.core.scheduler.api.SynchronizationException;
+import org.eclipse.dirigible.database.ds.model.IDataStructureModel;
 import org.eclipse.dirigible.repository.local.LocalResource;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -72,14 +84,16 @@ public class XSKHDBSchemaParserHanaITTest {
     String hanaUserName = Configuration.get("hana.username");
     ResultSet table = metaData.getTables(null, hanaUserName, "XSK_DATA_STRUCTURES", null);
     if (table.next()) {
-      stmt.executeUpdate(String.format("drop table \"%s\".\"XSK_DATA_STRUCTURES\"", hanaUserName));
+      stmt.executeUpdate(String.format("DELETE FROM \"%s\".\"XSK_DATA_STRUCTURES\" WHERE DS_LOCATION ='/hdbschema-itest/SampleHANAXSClassicSchema.hdbschema'", hanaUserName));
     }
   }
 
   @Test
-  public void testHDBSchemaCreate() throws XSKDataStructuresException, SynchronizationException, IOException, SQLException {
+  public void testHDBSchemaCreateWithNoCaseSensitivity() throws XSKDataStructuresException, SynchronizationException, IOException, SQLException {
+    Configuration.set(IDataStructureModel.DIRIGIBLE_DATABASE_NAMES_CASE_SENSITIVE, "false");
     Statement stmt = connection.createStatement();
     DatabaseMetaData metaData = connection.getMetaData();
+    String schemaName = "MYSCHEMA";//in hana the name will be created in UpperCase
 
     LocalResource resource = XSKHDBTestModule.getResources("/usr/local/target/dirigible/repository/root",
         "/registry/public/hdbschema-itest/SampleHANAXSClassicSchema.hdbschema",
@@ -88,8 +102,49 @@ public class XSKHDBSchemaParserHanaITTest {
     facade.handleResourceSynchronization(resource);
     facade.updateEntities();
 
-    ResultSet rs = metaData.getSchemas(null, "MYSCHEMA");
+    ResultSet rs = metaData.getSchemas(null, schemaName);
     assertTrue(rs.next());
-    stmt.executeUpdate(String.format("DROP SCHEMA %s", "MYSCHEMA"));
+    stmt.executeUpdate(String.format("DROP SCHEMA \"%s\"", schemaName));
+  }
+
+  @Test
+  public void testHDBSchemaCreate() throws XSKDataStructuresException, SynchronizationException, IOException, SQLException {
+    Configuration.set(IDataStructureModel.DIRIGIBLE_DATABASE_NAMES_CASE_SENSITIVE, "true");
+    Statement stmt = connection.createStatement();
+    DatabaseMetaData metaData = connection.getMetaData();
+    String schemaName = "MySchema";
+
+    LocalResource resource = XSKHDBTestModule.getResources("/usr/local/target/dirigible/repository/root",
+            "/registry/public/hdbschema-itest/SampleHANAXSClassicSchema.hdbschema",
+            "/hdbschema-itest/SampleHANAXSClassicSchema.hdbschema");
+
+    facade.handleResourceSynchronization(resource);
+    facade.updateEntities();
+
+    ResultSet rs = metaData.getSchemas(null, schemaName);
+    assertTrue(rs.next());
+    stmt.executeUpdate(String.format("DROP SCHEMA \"%s\"", schemaName));
+  }
+
+
+  @Test
+  public void testHDBSchemaCreateIfSchemaAlreadyExist() throws XSKDataStructuresException, SynchronizationException, IOException, SQLException {
+    Configuration.set(IDataStructureModel.DIRIGIBLE_DATABASE_NAMES_CASE_SENSITIVE, "true");
+    Statement stmt = connection.createStatement();
+    DatabaseMetaData metaData = connection.getMetaData();
+
+    String schemaName = "MySchema";
+    stmt.executeUpdate(String.format("create SCHEMA \"%s\"", schemaName));
+
+    LocalResource resource = XSKHDBTestModule.getResources("/usr/local/target/dirigible/repository/root",
+        "/registry/public/hdbschema-itest/SampleHANAXSClassicSchema.hdbschema",
+        "/hdbschema-itest/SampleHANAXSClassicSchema.hdbschema");
+
+    facade.handleResourceSynchronization(resource);
+    facade.updateEntities();
+
+    ResultSet rs = metaData.getSchemas(null, schemaName);
+    assertTrue(rs.next());
+    stmt.executeUpdate(String.format("DROP SCHEMA \"%s\"", schemaName));
   }
 }

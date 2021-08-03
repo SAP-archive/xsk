1475573ca63e8b9425ead3b779555bc8a95bbc1f
Nedelcho Delchev <delchevn@gmail.com>
2021-08-03 12:12:03 +0300

    cleanup

--- modules/engines/engine-xsodata/src/main/java/com/sap/xsk/xsodata/ds/module/XSKODataModule.java
+++ modules/engines/engine-xsodata/src/main/java/com/sap/xsk/xsodata/ds/module/XSKODataModule.java
@@ -12,23 +12,11 @@
 package com.sap.xsk.xsodata.ds.module;
 
 import org.eclipse.dirigible.commons.api.module.AbstractDirigibleModule;
-import org.eclipse.dirigible.commons.config.StaticObjects;
-
-import com.sap.xsk.xsodata.ds.dao.XSKODataArtifactDao;
-import com.sap.xsk.xsodata.ds.service.XSKODataCoreService;
-import com.sap.xsk.xsodata.ds.service.XSKODataParser;
 
 public class XSKODataModule extends AbstractDirigibleModule {
 
     @Override
     public void configure() {
-    	StaticObjects.set("xskXSODataArtifactDao", new XSKODataArtifactDao());
-    	StaticObjects.set("xskODataParser", new XSKODataParser());
-    	StaticObjects.set("xskODataCoreService", new XSKODataCoreService());
-    	
-//        bind(IXSKODataArtifactDao.class).annotatedWith(Names.named("xskXSODataArtifactDao")).to(XSKODataArtifactDao.class).in(Scopes.SINGLETON);
-//        bind(IXSKODataParser.class).annotatedWith(Names.named("xskODataParser")).to(XSKODataParser.class).in(Scopes.SINGLETON);
-//        bind(IXSKODataCoreService.class).annotatedWith(Names.named("xskODataCoreService")).to(XSKODataCoreService.class).in(Scopes.SINGLETON);
     }
 
     @Override

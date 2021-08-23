ac6737b3e50767cac2ee892dc653d1298b1905d6
antoanetamh <antoanetamh@gmail.com>
2021-08-23 11:32:47 +0300

    fix xsodata tests

--- modules/engines/engine-xsodata/src/itest/module/XSKHDBTestModule.java
+++ modules/engines/engine-xsodata/src/itest/module/XSKHDBTestModule.java
@@ -9,31 +9,22 @@
  * SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and XSK contributors
  * SPDX-License-Identifier: Apache-2.0
  */
-package com.sap.xsk.hdb.ds.itest.module;
+
+package module;
 
 import com.sap.xsk.hdb.ds.test.itest.model.JDBCModel;
-import com.sap.xsk.xsodata.ds.module.XSKODataModule;
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.eclipse.dirigible.commons.api.module.AbstractDirigibleModule;
 import org.eclipse.dirigible.commons.config.StaticObjects;
-import org.eclipse.dirigible.repository.api.RepositoryPath;
-import org.eclipse.dirigible.repository.fs.FileSystemRepository;
-import org.eclipse.dirigible.repository.local.LocalRepository;
-import org.eclipse.dirigible.repository.local.LocalResource;
 
 import javax.sql.DataSource;
-import java.io.IOException;
 
 public class XSKHDBTestModule extends AbstractDirigibleModule {
 
-    private JDBCModel model;
+    private final JDBCModel model;
 
     public XSKHDBTestModule(JDBCModel model) {
         this.model = model;
-    }
-
-    @Override
-    public void configure() {
-    	StaticObjects.set(StaticObjects.DATASOURCE, getDataSource); 
     }
 
     public DataSource getDataSource() {
@@ -45,17 +36,15 @@ public class XSKHDBTestModule extends AbstractDirigibleModule {
         basicDataSource.setDefaultAutoCommit(true);
         basicDataSource.setAccessToUnderlyingConnectionAllowed(true);
         return basicDataSource;
-
     }
 
-    public static LocalResource getResources(String rootFolder, String repoPath, String relativeResourcePath) throws IOException {
-        FileSystemRepository fileRepo = new LocalRepository(rootFolder);
-        RepositoryPath path = new RepositoryPath(repoPath);
-        byte[] content = XSKHDBTestModule.class
-                .getResourceAsStream(relativeResourcePath).readAllBytes();
+    @Override
+    public String getName() {
+        return "XSKHDBTestModule";
+    }
 
-        LocalResource resource = new LocalResource(fileRepo, path);
-        resource.setContent(content);
-        return resource;
+    @Override
+    public void configure() {
+        StaticObjects.set(StaticObjects.DATASOURCE, getDataSource());
     }
 }

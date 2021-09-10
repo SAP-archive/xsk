22080534a7a28bbff438f98ea2faa2c7da2de076
Vladimir Mutafov <vlado.mutafov@gmail.com>
2021-09-10 16:10:10 +0300

    chore: update xsk-ide-migration

--- modules/ide/ide-migration/src/main/resources/META-INF/dirigible/ide-migration/js/hana-credentials.js
+++ modules/ide/ide-migration/src/main/resources/META-INF/dirigible/ide-migration/js/hana-credentials.js
@@ -9,12 +9,91 @@
  * SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and XSK contributors
  * SPDX-License-Identifier: Apache-2.0
  */
-migrationLaunchView.controller('HanaCredentialsViewController', ['$scope', '$messageHub', function ($scope, $messageHub) {
+migrationLaunchView.controller('HanaCredentialsViewController', ['$scope', '$http', '$messageHub', function ($scope, $http, $messageHub) {
     $scope.isVisible = false;
     $scope.passwordVisible = false;
+    $scope.areDatabasesLoaded = false;
+    $scope.databasesDropdownText = "---Please select---";
+    $scope.databases = [];
+    $scope.databasesList = $scope.databases;
+    let processId = undefined;
+
+    let descriptionList = [
+        "Please wait while we get all available databases...",
+        "Provide the SAP HANA Credentials"
+    ];
+    $scope.descriptionText = descriptionList[0];
+    let neoData = undefined;
+
+    function getAvailableHanaDatabases() {
+        body = {
+            neo: neoData
+        }
+
+        $http.post(
+            "/services/v4/js/ide-migration/server/migration/api/migration-rest-api.js/start-process",
+            JSON.stringify(body),
+            { headers: { 'Content-Type': 'application/json' } }
+        ).then(function (response) {
+            processId = body.processInstanceId = response.data.processInstanceId;
+            const timer = setInterval(function () {
+                $http.post(
+                    "/services/v4/js/ide-migration/server/migration/api/migration-rest-api.js/get-process",
+                    JSON.stringify(body),
+                    { headers: { 'Content-Type': 'application/json' } }
+                ).then(function (response) {
+                    if (response.data.databases) {
+                        clearInterval(timer);
+                        $scope.areDatabasesLoaded = true;
+                        $scope.descriptionText = descriptionList[1];
+                        $scope.userInput();
+                        $scope.$parent.setPreviousVisible(true);
+                        $scope.$parent.setPreviousEnabled(true);
+                        $scope.$parent.setNextVisible(true);
+                        $scope.$parent.setNextEnabled(true);
+                        $scope.$parent.setFinishVisible(false);
+
+                        $scope.databasesDropdownText = "---Please select---";
+                        $scope.databases = response.data.databases;
+                        $scope.databasesList = $scope.databases;
+
+                    }
+                }, function (response) { })
+            }, 1000);
+
+        }, function (response) {
+            if (response.data) {
+                if ("error" in response.data) {
+                    if ("message" in response.data.error) {
+                        $messageHub.announceAlertError(
+                            defaultErrorTitle,
+                            response.data.error.message
+                        );
+                    } else {
+                        $messageHub.announceAlertError(
+                            defaultErrorTitle,
+                            defaultErrorDesc
+                        );
+                    }
+                    console.error(`HTTP $response.status`, response.data.error);
+                } else {
+                    $messageHub.announceAlertError(
+                        defaultErrorTitle,
+                        defaultErrorDesc
+                    );
+                }
+            } else {
+                $messageHub.announceAlertError(
+                    defaultErrorTitle,
+                    defaultErrorDesc
+                );
+            }
+            errorOccurred();
+        });
+    }
 
     $scope.userInput = function () {
-        if ($scope.schemaName && $scope.username && $scope.password) {
+        if ($scope.schemaName && $scope.username && $scope.password && $scope.areDatabasesLoaded) {
             $scope.$parent.setNextEnabled(true);
         } else {
             $scope.$parent.setNextEnabled(false);
@@ -25,18 +104,42 @@ migrationLaunchView.controller('HanaCredentialsViewController', ['$scope', '$mes
         $scope.passwordVisible = !$scope.passwordVisible;
     };
 
+    $scope.filterDatabases = function () {
+        if ($scope.databasesSearch) {
+            let filtered = [];
+            for (let i = 0; i < $scope.databases.length; i++) {
+                if ($scope.databases[i].toLowerCase().includes($scope.databasesSearch.toLowerCase())) {
+                    filtered.push($scope.databases[i]);
+                }
+            }
+            $scope.databasesList = filtered;
+        } else {
+            $scope.databasesList = $scope.databases;
+        }
+    };
+
+    $scope.databaseSelected = function (database) {
+        $scope.schemaName = database;
+        $scope.databasesDropdownText = database;
+    };
+
     $messageHub.on('migration.hana-credentials', function (msg) {
         if ("isVisible" in msg.data) {
             $scope.$apply(function () {
+                $scope.areDatabasesLoaded = false;
                 $scope.isVisible = msg.data.isVisible;
+                $scope.$parent.setPreviousVisible(false);
+                $scope.$parent.setPreviousEnabled(false);
+                $scope.$parent.setNextVisible(false);
+                $scope.$parent.setNextEnabled(false);
                 if (msg.data.isVisible) {
-                    $scope.userInput();
-                    $scope.$parent.setPreviousVisible(true);
-                    $scope.$parent.setPreviousEnabled(true);
-                    $scope.$parent.setNextVisible(true);
-                    $scope.$parent.setFinishVisible(false);
+                    $messageHub.message('migration.neo-credentials', { controller: "migration.hana-credentials", getData: "all" });
                 }
             });
+        }
+        if ("neoData" in msg.data) {
+            neoData = msg.data.neoData;
+            getAvailableHanaDatabases();
         }
         if ("getData" in msg.data) {
             if (msg.data.getData === "all") {
@@ -44,7 +147,8 @@ migrationLaunchView.controller('HanaCredentialsViewController', ['$scope', '$mes
                     hanaData: {
                         "databaseSchema": $scope.schemaName,
                         "username": $scope.username,
-                        "password": $scope.password
+                        "password": $scope.password,
+                        "processId": processId
                     }
                 });
             }

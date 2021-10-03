5f453bccc6b4e083f53a3de302d4ea9f4b24d3e3
aihanrashidov <aihan_rashidov@mail.bg>
2021-10-03 10:08:37 +0300

    Added config for smtp connection and mail send method

--- modules/api/api-xsjs/src/main/resources/META-INF/dirigible/xsk/net/net.js
+++ modules/api/api-xsjs/src/main/resources/META-INF/dirigible/xsk/net/net.js
@@ -14,41 +14,40 @@ exports.http = require("xsk/http/http");
 var mail = require("mail/v4/client");
 
 exports.Mail = function (mailObject) {
-    this.to = mailObject.to || [];
-    this.cc = mailObject.cc || [];
-    this.bcc = mailObject.bcc || [];
-    this.parts = mailObject.parts || [];
+  this.to = mailObject.to || [];
+  this.cc = mailObject.cc || [];
+  this.bcc = mailObject.bcc || [];
+  this.parts = mailObject.parts || [];
 
-    this.subject = mailObject.subject;
-    this.sender = mailObject.sender || { name: "", address: "", nameEncoding: "" };
-    this.subjectEncoding = mailObject.subjectEncoding;
+  this.subject = mailObject.subject;
+  this.sender = mailObject.sender || {name: "", address: "", nameEncoding: ""};
+  this.subjectEncoding = mailObject.subjectEncoding;
 
+  this.send = function (mailConfig) {
+    let mailClient = mail.getClient(mailConfig || {});
+    let recipients = {
+      to: this.to.map(e => e.address),
+      cc: this.cc.map(e => e.address),
+      bcc: this.bcc.map(e => e.address)
+    };
 
-    this.send = function () {
-        let mailClient = mail.getClient();
-        let recipients = {
-            to: this.to.map(e => e.address),
-            cc: this.cc.map(e => e.address),
-            bcc: this.bcc.map(e => e.address)
-        };
+    mailClient.send(this.sender.address, recipients, this.subject, this.parts[0].text, adaptMailContentType(this.parts[0]));
 
-        mailClient.send(this.sender.address, recipients, this.subject, this.parts[0].text, adaptMailContentType(this.parts[0]));
-
-        //Mocked in order to avoid null exceptions.
-        return {"finalReply": "NOT SUPPORTED", "messageId": 1};
-    }
+    //Mocked in order to avoid null exceptions.
+    return {"finalReply": "NOT SUPPORTED", "messageId": 1};
+  }
 }
 exports.Mail.Part = function (partObject) {
-    this.alternative = partObject && partObject.alternative;
-    this.alternativeContentType = partObject && partObject.alternativeContentType;
-    this.contentId = partObject && partObject.contentId;
-    this.contentType = partObject && partObject.contentType;
-    this.data = partObject && partObject.data;
-    this.encoding = partObject && partObject.encoding;
-    this.fileName = partObject && partObject.fileName;
-    this.fileNameEncoding = partObject && partObject.fileNameEncoding;
-    this.text = partObject && partObject.text;
-    this.type = partObject && partObject.type;
+  this.alternative = partObject && partObject.alternative;
+  this.alternativeContentType = partObject && partObject.alternativeContentType;
+  this.contentId = partObject && partObject.contentId;
+  this.contentType = partObject && partObject.contentType;
+  this.data = partObject && partObject.data;
+  this.encoding = partObject && partObject.encoding;
+  this.fileName = partObject && partObject.fileName;
+  this.fileNameEncoding = partObject && partObject.fileNameEncoding;
+  this.text = partObject && partObject.text;
+  this.type = partObject && partObject.type;
 };
 
 //The values of the properties are unknown and not actually needed. Mocked in order to avoid null exceptions.
@@ -56,19 +55,15 @@ exports.Mail.Part.TYPE_TEXT = "ToDO";
 exports.Mail.Part.TYPE_ATTACHMENT = "ToDO";
 exports.Mail.Part.TYPE_INLINE = "ToDO";
 
-exports.Destination = function(packageName, objectName) {
+exports.Destination = function (packageName, objectName) {
   let destination = exports.http.readDestination(packageName, objectName);
   this.host = destination.host || "";
   this.port = destination.port || "";
 }
 
-exports.SMTPConnection = function() {
-  this.send = function(mailClass) {
-    let mailConfig = {
-      "mail.transport.protocol": "smtp",
-      "mail.smtp.port": "587"
-    };
-    let mailClient = mail.getClient(mailConfig);
+exports.SMTPConnection = function (mailConfig) {
+  this.send = function (mailClass) {
+    let mailClient = mail.getClient(mailConfig || {});
     let recipients = {
       to: mailClass.to.map(e => e.address),
       cc: mailClass.cc.map(e => e.address),
@@ -77,19 +72,21 @@ exports.SMTPConnection = function() {
 
     mailClient.send(mailClass.sender.address, recipients, mailClass.subject, mailClass.parts[0].text, adaptMailContentType(mailClass.parts[0]));
   }
-  this.close = function() {
+  this.close = function () {
     // Empty. Called automatically inside MailFacade
   }
-  this.isClosed = function() {
+  this.isClosed = function () {
     // The connection is being closed automatically
     return true;
   }
 }
 
-function adaptMailContentType(part){
-  switch(part.contentType){
-    case "text/html" : return "html";
-    case "text/plain": return "plain"
+function adaptMailContentType(part) {
+  switch (part.contentType) {
+    case "text/html" :
+      return "html";
+    case "text/plain":
+      return "plain"
   }
   return part.contentType;
 }

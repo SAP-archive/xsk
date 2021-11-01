9082c3069f1f60e574d4228b51c725a073ecf24d
aihanrashidov <aihan_rashidov@mail.bg>
2021-11-01 17:03:51 +0200

    Fixed uploading xsodata artifacts to workspace

--- modules/engines/engine-xsodata/src/main/java/com/sap/xsk/xsodata/ds/filter/XSODataForwardFilter.java
+++ modules/engines/engine-xsodata/src/main/java/com/sap/xsk/xsodata/ds/filter/XSODataForwardFilter.java
@@ -19,34 +19,43 @@ import java.io.IOException;
 @WebFilter("/*")
 public class XSODataForwardFilter implements Filter {
 
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        //
-    }
+  @Override
+  public void init(FilterConfig filterConfig) throws ServletException {
+    //
+  }
 
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
-        //only on production case
-        if (httpServletRequest.getHeader("Dirigible-Editor") == null) {
-            String uri = httpServletRequest.getRequestURI();
-            int index = uri.indexOf(".xsodata");
-            if (index > 0) {
-                String parameters = "";
-                if (uri.length() > index + (".xsodata".length() - 1)) {
-                    parameters = uri.substring(index + ".xsodata".length());
-                }
-                RequestDispatcher dispatcher = request.getRequestDispatcher("/odata/v2/" + parameters);
-                dispatcher.forward(request, response);
-            }
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+      throws IOException, ServletException {
+    WrappedHttpServletRequest wrappedHttpServletRequest = new WrappedHttpServletRequest((HttpServletRequest) request);
+
+    //only on production case
+
+    String editorHeader = wrappedHttpServletRequest.getHeader("Dirigible-Editor");
+    String requestMethod = wrappedHttpServletRequest.getMethod();
+
+    if (editorHeader == null && requestMethod.equals("POST")) {
+      wrappedHttpServletRequest.putHeader("Dirigible-Editor", "Workspace");
+    } else if (editorHeader == null) {
+      String uri = wrappedHttpServletRequest.getRequestURI();
+      int index = uri.indexOf(".xsodata");
+      if (index > 0) {
+        String parameters = "";
+        if (uri.length() > index + (".xsodata".length() - 1)) {
+          parameters = uri.substring(index + ".xsodata".length());
         }
-        chain.doFilter(request, response);
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/odata/v2/" + parameters);
+        dispatcher.forward(request, response);
+      }
     }
 
-    @Override
-    public void destroy() {
-        //
-    }
+    chain.doFilter(wrappedHttpServletRequest, response);
+  }
+
+  @Override
+  public void destroy() {
+    //
+  }
 
 }

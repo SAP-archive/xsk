8b82a9f22f6adab6d47a63ed591454f8ccedfcbd
Ivan <iwolkkov@gmail.com>
2021-11-04 09:31:24 +0200

    replace Javasctipt service option with XSJS service.

---
+++ modules/ide/xsk-workspace-menu/src/main/resources/META-INF/dirigible/xsk-workspace-menu/services/workspace-menu-new-template-html.js
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2021 SAP SE or an SAP affiliate company and XSK contributors
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License, v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and XSK contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+exports.getTemplate = function() {
+	return {
+			"name":"html",
+			"label":"HTML5 Page",
+			"extension":"html",
+			"data":'<!DOCTYPE html>\n<head>\n</head>\n<body>\n</body>\n</html>'
+	};
+};

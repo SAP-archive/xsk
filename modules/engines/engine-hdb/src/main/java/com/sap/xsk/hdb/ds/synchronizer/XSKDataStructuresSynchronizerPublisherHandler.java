52a782d162625e33cad63cdac3023d8d8e5c37aa
cheezus1 <bobinikolov33@gmail.com>
2021-12-09 17:52:27 +0200

    Unpublish now deletes related artefacts metadata

---
+++ modules/engines/engine-hdb/src/main/java/com/sap/xsk/hdb/ds/synchronizer/XSKDataStructuresSynchronizerPublisherHandler.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2021 SAP SE or an SAP affiliate company and XSK contributors
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License, v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and XSK contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package com.sap.xsk.hdb.ds.synchronizer;
+
+import com.sap.xsk.hdb.ds.model.XSKDataStructureModel;
+import org.eclipse.dirigible.core.publisher.api.PublisherHandler;
+import org.eclipse.dirigible.core.scheduler.api.SchedulerException;
+import org.eclipse.dirigible.database.persistence.PersistenceManager;
+
+public class XSKDataStructuresSynchronizerPublisherHandler extends PublisherHandler {
+
+  @Override
+  public void beforePublish(String location) {
+
+  }
+
+  @Override
+  public void afterPublish(String location) {
+
+  }
+
+  @Override
+  public void beforeUnpublish(String location) {
+
+  }
+
+  @Override
+  public void afterUnpublish(String location) throws SchedulerException {
+    String locationQueryParam = getLocationQueryParam(location, true);
+
+    removeMetadata(new PersistenceManager<XSKDataStructureModel>(), "XSK_DATA_STRUCTURES", "DS_LOCATION", locationQueryParam);
+  }
+}

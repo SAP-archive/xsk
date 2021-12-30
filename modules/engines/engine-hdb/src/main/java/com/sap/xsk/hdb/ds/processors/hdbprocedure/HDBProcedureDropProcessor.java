8d49542b688af21c1e65c80df22ad9eeace00d9b
Liliya Lazarova <>
2021-12-30 12:14:30 +0200

    #768_keep sync state hdbprocedure

--- modules/engines/engine-hdb/src/main/java/com/sap/xsk/hdb/ds/processors/hdbprocedure/HDBProcedureDropProcessor.java
+++ modules/engines/engine-hdb/src/main/java/com/sap/xsk/hdb/ds/processors/hdbprocedure/HDBProcedureDropProcessor.java
@@ -13,13 +13,16 @@ package com.sap.xsk.hdb.ds.processors.hdbprocedure;
 
 import static java.text.MessageFormat.format;
 
+import com.sap.xsk.hdb.ds.artefacts.HDBProcedureSynchronizationArtefactType;
 import com.sap.xsk.hdb.ds.model.hdbprocedure.XSKDataStructureHDBProcedureModel;
 import com.sap.xsk.hdb.ds.processors.AbstractXSKProcessor;
+import com.sap.xsk.hdb.ds.synchronizer.XSKDataStructuresSynchronizer;
 import com.sap.xsk.utils.XSKCommonsConstants;
 import com.sap.xsk.utils.XSKCommonsUtils;
 import com.sap.xsk.utils.XSKConstants;
 import java.sql.Connection;
 import java.sql.SQLException;
+import org.eclipse.dirigible.core.scheduler.api.ISynchronizerArtefactType.ArtefactState;
 import org.eclipse.dirigible.database.sql.DatabaseArtifactTypes;
 import org.eclipse.dirigible.database.sql.ISqlDialect;
 import org.eclipse.dirigible.database.sql.SqlFactory;
@@ -30,6 +33,8 @@ import org.slf4j.LoggerFactory;
 public class HDBProcedureDropProcessor extends AbstractXSKProcessor<XSKDataStructureHDBProcedureModel> {
 
   private static final Logger logger = LoggerFactory.getLogger(HDBProcedureDropProcessor.class);
+  private static final HDBProcedureSynchronizationArtefactType PROCEDURE_ARTEFACT = new HDBProcedureSynchronizationArtefactType();
+  private static final XSKDataStructuresSynchronizer dataStructuresSynchronizer = new XSKDataStructuresSynchronizer();
 
   public void execute(Connection connection, XSKDataStructureHDBProcedureModel hdbProcedure)
       throws SQLException {
@@ -42,17 +47,24 @@ public class HDBProcedureDropProcessor extends AbstractXSKProcessor<XSKDataStruc
       if (!(dialect.getClass().equals(HanaSqlDialect.class))) {
         String errorMessage = String.format("Procedures are not supported for %s", dialect.getDatabaseName(connection));
         XSKCommonsUtils.logProcessorErrors(errorMessage, XSKCommonsConstants.PROCESSOR_ERROR, hdbProcedure.getLocation(), XSKCommonsConstants.HDB_PROCEDURE_PARSER);
+        dataStructuresSynchronizer.applyArtefactState(hdbProcedure.getName(), hdbProcedure.getLocation(), PROCEDURE_ARTEFACT, ArtefactState.FAILED_DELETE, errorMessage);
         throw new IllegalStateException(errorMessage);
       } else {
         String sql = XSKConstants.XSK_HDBPROCEDURE_DROP + hdbProcedure.getName();
         try {
           executeSql(sql, connection);
+          String message = String.format("Drop procedure %s successfully", hdbProcedure.getName());
+          dataStructuresSynchronizer.applyArtefactState(hdbProcedure.getName(), hdbProcedure.getLocation(), PROCEDURE_ARTEFACT, ArtefactState.SUCCESSFUL_DELETE, message);
         } catch (SQLException ex) {
+          String message = String.format("Drop procedure[%s] skipped due to an error: %s", hdbProcedure, ex.getMessage());
           XSKCommonsUtils.logProcessorErrors(ex.getMessage(), XSKCommonsConstants.PROCESSOR_ERROR, hdbProcedure.getLocation(), XSKCommonsConstants.HDB_PROCEDURE_PARSER);
+          dataStructuresSynchronizer.applyArtefactState(hdbProcedure.getName(), hdbProcedure.getLocation(), PROCEDURE_ARTEFACT, ArtefactState.FAILED_DELETE, message);
         }
       }
     } else {
-      logger.warn(format("Procedure [{0}] does not exists during the drop process", hdbProcedure.getName()));
+      String warningMessage = String.format("Procedure [%s] does not exists during the drop process", hdbProcedure.getName());
+      logger.warn(warningMessage);
+      dataStructuresSynchronizer.applyArtefactState(hdbProcedure.getName(), hdbProcedure.getLocation(), PROCEDURE_ARTEFACT, ArtefactState.FAILED_DELETE, warningMessage);
     }
   }
 }

6a00587c79f3bc49e10f0f90b1828bda943b945b
Ivan Volkov <61655573+ivanvolkoff@users.noreply.github.com>
2022-01-11 15:37:51 +0200

    feat: add migrations project modification hook (#984)
    * in progres
    
    * add files
    
    * chore: refactor some of the XSK project modifications hook
    
    * ide-migration module update
    
    * remove excessive .xslt
    
    Co-authored-by: Vladimir Mutafov <vlado.mutafov@gmail.com>

--- modules/ide/ide-migration/src/main/resources/META-INF/dirigible/ide-migration/server/migration/api/migration-tool-executor.js
+++ modules/ide/ide-migration/src/main/resources/META-INF/dirigible/ide-migration/server/migration/api/migration-tool-executor.js
@@ -1,14 +1,3 @@
-/*
- * Copyright (c) 2021 SAP SE or an SAP affiliate company and XSK contributors
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Apache License, v2.0
- * which accompanies this distribution, and is available at
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and XSK contributors
- * SPDX-License-Identifier: Apache-2.0
- */
 const exec = require("core/v4/exec");
 const config = require("core/v4/configurations");
 
--- modules/ide/ide-migration/src/main/resources/META-INF/dirigible/ide-migration/server/migration/process/populate-projects-task.js
+++ modules/ide/ide-migration/src/main/resources/META-INF/dirigible/ide-migration/server/migration/process/populate-projects-task.js
@@ -1,14 +1,3 @@
-/*
- * Copyright (c) 2021 SAP SE or an SAP affiliate company and XSK contributors
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Apache License, v2.0
- * which accompanies this distribution, and is available at
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and XSK contributors
- * SPDX-License-Identifier: Apache-2.0
- */
 const process = require('bpm/v4/process');
 const execution = process.getExecutionContext();
 const MigrationService = require('ide-migration/server/migration/api/migration-service');
@@ -17,6 +6,8 @@ const repositoryManager = require("platform/v4/repository");
 
 const TrackService = require('ide-migration/server/migration/api/track-service');
 const trackService = new TrackService();
+const XSKProjectMigrationInterceptor = Java.type("com.sap.xsk.modificators.XSKProjectMigrationInterceptor")
+
 try {
 	process.setVariable(execution.getId(), 'migrationState', 'POPULATING_PROJECTS');
 	trackService.updateMigrationStatus('POPULATING PROJECTS');
@@ -25,6 +16,7 @@ try {
 
 	const migrationService = new MigrationService();
 	const workspace = userData.workspace;
+    const modificator = new XSKProjectMigrationInterceptor();
 
     for (const deliveryUnit of userData.du) {
         const locals = deliveryUnit.locals;
@@ -34,6 +26,8 @@ try {
         for (const local of locals) {
             migrationService.addFileToWorkspace(workspace, local.repositoryPath, local.relativePath, local.projectName);
         }
+        const projectName = locals[0].projectName;
+        modificator.interceptXSKProject(workspace, projectName);
         git.initRepository('migration', '', workspace, locals[0].projectName, locals[0].projectName, "Migration initial commit");
 
         const generated = deliveryUnit['deployableArtifactsResult']['generated'];
@@ -42,9 +36,6 @@ try {
         }
         let repositoryName = locals[0].projectName;
         git.commit('migration', '', userData.workspace, repositoryName, 'Artifacts handled', true);
-
-        repositoryManager.deleteCollection(userData.workspace);
-
     }
     process.setVariable(execution.getId(), 'migrationState', 'MIGRATION_EXECUTED');
 	trackService.updateMigrationStatus('MIGRATION EXECUTED');

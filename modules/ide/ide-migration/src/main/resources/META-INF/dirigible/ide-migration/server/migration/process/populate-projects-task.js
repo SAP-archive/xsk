d16727c8b0ea793ca34605956d4d2e8eac8585b5
Vladimir Mutafov <vlado.mutafov@gmail.com>
2022-01-12 16:18:07 +0200

    chore: update ide migration

--- modules/ide/ide-migration/src/main/resources/META-INF/dirigible/ide-migration/server/migration/process/populate-projects-task.js
+++ modules/ide/ide-migration/src/main/resources/META-INF/dirigible/ide-migration/server/migration/process/populate-projects-task.js
@@ -12,8 +12,9 @@
 const process = require('bpm/v4/process');
 const execution = process.getExecutionContext();
 const MigrationService = require('ide-migration/server/migration/api/migration-service');
-const git = require('git/v4/client');
+// const git = require('git/v4/client'); 
 const repositoryManager = require("platform/v4/repository");
+
 const TrackService = require('ide-migration/server/migration/api/track-service');
 const trackService = new TrackService();
 const XSKProjectMigrationInterceptor = Java.type("com.sap.xsk.modificators.XSKProjectMigrationInterceptor")
@@ -38,14 +39,14 @@ try {
         }
         const projectName = locals[0].projectName;
         modificator.interceptXSKProject(workspace, projectName);
-        git.initRepository('migration', '', workspace, locals[0].projectName, locals[0].projectName, "Migration initial commit");
+        org.eclipse.dirigible.api.v4.git.GitFacade.initRepository('migration', '', workspace, locals[0].projectName, locals[0].projectName, "Migration initial commit");
 
         const generated = deliveryUnit['deployableArtifactsResult']['generated'];
         for (const gen of generated) {
             migrationService.addFileToWorkspace(workspace, gen.repositoryPath, gen.relativePath, locals[0].projectName);
         }
         let repositoryName = locals[0].projectName;
-        git.commit('migration', '', userData.workspace, repositoryName, 'Artifacts handled', true);
+        org.eclipse.dirigible.api.v4.git.GitFacade.commit('migration', '', userData.workspace, repositoryName, 'Artifacts handled', true);
     }
     process.setVariable(execution.getId(), 'migrationState', 'MIGRATION_EXECUTED');
 	trackService.updateMigrationStatus('MIGRATION EXECUTED');

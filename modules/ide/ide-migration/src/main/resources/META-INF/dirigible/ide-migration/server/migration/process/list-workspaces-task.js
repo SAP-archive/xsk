a4c259e44f42420b2f2f0b4ad9eca90e6958ee60
ThuF <jordan.p.pavlov@gmail.com>
2022-01-12 10:11:44 +0200

    License fix

--- modules/ide/ide-migration/src/main/resources/META-INF/dirigible/ide-migration/server/migration/process/list-workspaces-task.js
+++ modules/ide/ide-migration/src/main/resources/META-INF/dirigible/ide-migration/server/migration/process/list-workspaces-task.js
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2021 SAP SE or an SAP affiliate company and XSK contributors
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License, v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and XSK contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
 const process = require('bpm/v4/process');
 const execution = process.getExecutionContext();
 const workspaceManager = require('platform/v4/workspace');

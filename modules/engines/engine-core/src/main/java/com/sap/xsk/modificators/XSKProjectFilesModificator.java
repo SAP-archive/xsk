c5022f5bbe0f4c3c338f65af280957cf63131c6e
aihanrashidov <aihan_rashidov@mail.bg>
2022-01-18 15:48:16 +0200

    Resolving pr comments

--- modules/engines/engine-core/src/main/java/com/sap/xsk/modificators/XSKProjectFilesModificator.java
+++ modules/engines/engine-core/src/main/java/com/sap/xsk/modificators/XSKProjectFilesModificator.java
@@ -11,19 +11,20 @@
  */
 package com.sap.xsk.modificators;
 
+import java.io.IOException;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.eclipse.dirigible.core.workspace.api.IFile;
-import org.eclipse.dirigible.core.workspace.api.IProject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -31,70 +32,60 @@ public class XSKProjectFilesModificator {
 
   private static final Logger logger = LoggerFactory.getLogger(XSKProjectFilesModificator.class);
 
-  private static String XSLT_RESOURCE_PATH = "META-INF/modificators/xslt/analyticprivilege.xslt";
+  private static final String XSLT_RESOURCE_PATH = "META-INF/modificators/xslt/analyticprivilege.xslt";
+  private static final List<String> REPLACE_SESSION_USER_FILE_EXTENSIONS = List.of("xsjs", "xsjslib", "hdbprocedure", "hdbtablefunction",
+      "analyticprivilege", "hdbanalyticprivilege");
+  private static final List<String> ANALYTIC_PRIVILEGE_FILE_EXTENSIONS = List.of("analyticprivilege", "hdbanalyticprivilege");
 
-  public void modifyProjectFiles(IProject project, List<IFile> projectFiles) {
+  public void modifyProjectFiles(List<IFile> projectFiles) throws IOException {
     for (IFile projectFile : projectFiles) {
-
       modifyProjectFileIfNecessary(projectFile);
-      addNewProjectFileIfNecessary(project);
     }
   }
 
-  private void modifyProjectFileIfNecessary(IFile projectFile) {
+  private void modifyProjectFileIfNecessary(IFile projectFile) throws IOException {
     String fileExtension = getProjectFileExtension(projectFile);
 
-    switch (fileExtension) {
-      case "xsjs":
-      case "xsjslib":
-      case "hdbprocedure":
-      case "hdbtablefunction": {
-        replaceSessionUser(projectFile);
-        break;
-      }
-      case "analyticprivilege":
-      case "hdbanalyticprivilege": {
-        replaceSessionUser(projectFile);
-        modifyAnalyticPrivilegeFile(projectFile);
-        break;
-      }
+    replaceSessionUser(fileExtension, projectFile);
+    modifyAnalyticPrivilegeFile(fileExtension, projectFile);
+  }
+
+  private void replaceSessionUser(String fileExtension, IFile projectFile) {
+    if (REPLACE_SESSION_USER_FILE_EXTENSIONS.contains(fileExtension)) {
+      byte[] currentContent = projectFile.getContent();
+      projectFile.setContent(new String(currentContent).replace("SESSION_USER", "SESSION_CONTEXT(APPLICATIONUSER)").getBytes());
     }
   }
 
-  private void addNewProjectFileIfNecessary(IProject project) {
-  }
+  private void modifyAnalyticPrivilegeFile(String fileExtension, IFile analyticPrivilegeFile) throws IOException {
+    if (ANALYTIC_PRIVILEGE_FILE_EXTENSIONS.contains(fileExtension)) {
+      byte[] currentContent = analyticPrivilegeFile.getContent();
 
-  private void replaceSessionUser(IFile projectFile) {
-    byte[] currentContent = projectFile.getContent();
-    projectFile.setContent(new String(currentContent).replace("SESSION_USER", "SESSION_CONTEXT(APPLICATIONUSER)").getBytes());
-  }
+      if (currentContent.length != 0) {
+        try {
+          String xslt = IOUtils.toString(
+              XSKProjectFilesModificator.class.getClassLoader().getResourceAsStream(XSLT_RESOURCE_PATH),
+              StandardCharsets.UTF_8);
 
-  private void modifyAnalyticPrivilegeFile(IFile analyticPrivilegeFile) {
-    byte[] currentContent = analyticPrivilegeFile.getContent();
+          StringWriter writer = new StringWriter();
+          StreamResult result = new StreamResult(writer);
 
-    if (currentContent.length != 0) {
-      try {
-        String xslt = IOUtils.toString(
-            XSKProjectFilesModificator.class.getClassLoader().getResourceAsStream(XSLT_RESOURCE_PATH),
-            StandardCharsets.UTF_8);
+          TransformerFactory factory = TransformerFactory.newInstance();
+          Source xsltSource = new StreamSource(new StringReader(xslt));
+          Transformer transformer = factory.newTransformer(xsltSource);
 
-        StringWriter writer = new StringWriter();
-        StreamResult result = new StreamResult(writer);
+          Source contentSource = new StreamSource(new StringReader(new String(currentContent)));
+          transformer.transform(contentSource, result);
 
-        TransformerFactory factory = TransformerFactory.newInstance();
-        Source xsltSource = new StreamSource(new StringReader(xslt));
-        Transformer transformer = factory.newTransformer(xsltSource);
+          String processedAnalyticPrivilege = writer.toString();
 
-        Source contentSource = new StreamSource(new StringReader(new String(currentContent)));
-        transformer.transform(contentSource, result);
-
-        String processedAnalyticPrivilege = writer.toString();
-
-        analyticPrivilegeFile.setContent(processedAnalyticPrivilege.getBytes());
-      } catch (Exception e) {
-        logger.error("Incorrect analytic privilege file content format.");
+          analyticPrivilegeFile.setContent(processedAnalyticPrivilege.getBytes());
+        } catch (TransformerException exception) {
+          String errorMessage = String.format("Analytic privilege [%s] modification has failed due to an error: %s",
+              analyticPrivilegeFile.getName(), exception.getMessage());
+          logger.error(errorMessage, exception);
+        }
       }
-
     }
   }
 
@@ -103,13 +94,13 @@ public class XSKProjectFilesModificator {
   }
 
   public static String processModelUri(String value) {
-    int start = 0;
+    boolean uriIsNotWithPackageAndFolder = value.indexOf("/") == -1;
 
-    if (value.indexOf("/") != -1) {
-      start = value.lastIndexOf("/") + 1;
+    if (uriIsNotWithPackageAndFolder) {
+      return value;
     }
 
-    return value.substring(start);
+    return value.substring(value.lastIndexOf("/") + 1);
   }
 
   public static String processWhereSql(String value) {
--- modules/engines/engine-core/src/test/java/com/sap/xsk/modificators/XSKProjectFilesModificatorTest.java
+++ modules/engines/engine-core/src/test/java/com/sap/xsk/modificators/XSKProjectFilesModificatorTest.java
@@ -15,10 +15,8 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
-import org.apache.commons.io.IOUtils;
 import org.eclipse.dirigible.api.v3.platform.WorkspaceFacade;
 import org.eclipse.dirigible.core.test.AbstractDirigibleTest;
 import org.eclipse.dirigible.core.workspace.api.IFile;
@@ -41,184 +39,186 @@ public class XSKProjectFilesModificatorTest extends AbstractDirigibleTest {
   }
 
   @Test
-  public void modifyProjectFilesSuccessfully() throws IOException {
-    List<IFile> projectFiles = new ArrayList<>();
-
-    byte[] xsjsFile = IOUtils.toString(XSKProjectFilesModificatorTest.class.getResourceAsStream("/META-INF/modificators/files-to-modify/test.xsjs"),
-        StandardCharsets.UTF_8).getBytes();
-    IFile xsjsIFile = project.createFile("test.xsjs", xsjsFile);
-
-    byte[] xsjslibFile = IOUtils.toString(
-        XSKProjectFilesModificatorTest.class.getResourceAsStream("/META-INF/modificators/files-to-modify/test.xsjslib"),
-        StandardCharsets.UTF_8).getBytes();
-    IFile xsjslibIFile = project.createFile("test.xsjslib", xsjslibFile);
-
-    byte[] hdbprocedureFile = IOUtils.toString(XSKProjectFilesModificatorTest.class.getResourceAsStream(
-            "/META-INF/modificators/files-to-modify/test.hdbprocedure"),
-        StandardCharsets.UTF_8).getBytes();
-    IFile hdbprocedureIFile = project.createFile("test.hdbprocedure", hdbprocedureFile);
-
-    byte[] hdbtablefunctionFile = IOUtils.toString(XSKProjectFilesModificatorTest.class.getResourceAsStream(
-            "/META-INF/modificators/files-to-modify/test.hdbtablefunction"),
-        StandardCharsets.UTF_8).getBytes();
-    IFile hdbtablefunctionIFile = project.createFile("test.hdbtablefunction", hdbtablefunctionFile);
-
-    byte[] analyticPrivilegeFile = IOUtils.toString(
-        XSKProjectFilesModificatorTest.class.getResourceAsStream("/META-INF/modificators/files-to-modify/test.analyticprivilege"),
-        StandardCharsets.UTF_8).getBytes();
-    IFile analyticPrivilegeIFile = project.createFile("test.analyticprivilege", analyticPrivilegeFile);
-
-    byte[] hdbanalyticPrivilegeFile = IOUtils.toString(
-        XSKProjectFilesModificatorTest.class.getResourceAsStream("/META-INF/modificators/files-to-modify/test.hdbanalyticprivilege"),
-        StandardCharsets.UTF_8).getBytes();
-    IFile hdbanalyticPrivilegeIFile = project.createFile("test.hdbanalyticprivilege", hdbanalyticPrivilegeFile);
-
-    projectFiles.add(xsjsIFile);
-    projectFiles.add(xsjslibIFile);
-    projectFiles.add(hdbprocedureIFile);
-    projectFiles.add(hdbtablefunctionIFile);
-    projectFiles.add(analyticPrivilegeIFile);
-    projectFiles.add(hdbanalyticPrivilegeIFile);
-
-    projectFilesModificator.modifyProjectFiles(project, projectFiles);
-
-    byte[] expectedModifiedXsjsFile = XSKProjectFilesModificatorTest.class.getResourceAsStream(
-        "/META-INF/modificators/expected-results/test.xsjs").readAllBytes();
-    byte[] expectedModifiedXsjslibFile = XSKProjectFilesModificatorTest.class.getResourceAsStream(
-        "/META-INF/modificators/expected-results/test.xsjslib").readAllBytes();
-    byte[] expectedModifiedHdbProcedureFile = XSKProjectFilesModificatorTest.class.getResourceAsStream(
-        "/META-INF/modificators/expected-results/test.hdbprocedure").readAllBytes();
-    byte[] expectedModifiedHdbtablefunctionFile = XSKProjectFilesModificatorTest.class.getResourceAsStream(
-        "/META-INF/modificators/expected-results/test.hdbtablefunction").readAllBytes();
-    byte[] expectedModifiedAnalyticprivilegeFile = XSKProjectFilesModificatorTest.class.getResourceAsStream(
-        "/META-INF/modificators/expected-results/test.analyticprivilege").readAllBytes();
-    byte[] expectedModifiedHdbanalyticprivilegeFile = XSKProjectFilesModificatorTest.class.getResourceAsStream(
-        "/META-INF/modificators/expected-results/test.hdbanalyticprivilege").readAllBytes();
-
-    for (IFile projectFile : projectFiles) {
-      switch (projectFile.getName()) {
-        case "test.xsjs": {
-          assertArrayEquals(projectFile.getContent(), expectedModifiedXsjsFile);
-          break;
-        }
-        case "test.xsjslib": {
-          assertArrayEquals(projectFile.getContent(), expectedModifiedXsjslibFile);
-          break;
-        }
-        case "test.hdbprocedure": {
-          assertArrayEquals(projectFile.getContent(), expectedModifiedHdbProcedureFile);
-          break;
-        }
-        case "test.hdbtablefunction": {
-          assertArrayEquals(projectFile.getContent(), expectedModifiedHdbtablefunctionFile);
-          break;
-        }
-        case "test.analyticprivilege": {
-          assertArrayEquals(projectFile.getContent(), expectedModifiedAnalyticprivilegeFile);
-          break;
-        }
-        case "test.hdbanalyticprivilege": {
-          assertArrayEquals(projectFile.getContent(), expectedModifiedHdbanalyticprivilegeFile);
-          break;
-        }
-      }
-    }
+  public void testModifyPorjectFilesSuccessfullyWhenXsjs() throws IOException {
+    modifyProjectFilesAssertArrayEqualsWhenModified("testSuccessful.xsjs");
   }
 
   @Test
-  public void modifyProjectFilesWithEmptyContent() {
-    List<IFile> projectFiles = new ArrayList<>();
-
-    IFile emptyContentIFile = project.createFile("test.xsjs", new byte[0]);
-    projectFiles.add(emptyContentIFile);
-    projectFilesModificator.modifyProjectFiles(project, projectFiles);
-
-    assertEquals(projectFiles.get(0).getContent().length, 0);
+  public void testModifyProjectFilesSuccessfullyWhenXsjslib() throws IOException {
+    modifyProjectFilesAssertArrayEqualsWhenModified("testSuccessful.xsjslib");
   }
 
   @Test
-  public void modifyProjectFilesWithWhenAnalyticPrivilegeContentIsEmpty() {
-    List<IFile> projectFiles = new ArrayList<>();
-
-    IFile emptyContentAnalyticPrivilegeIFile = project.createFile("test.analyticprivilege", new byte[0]);
-    projectFiles.add(emptyContentAnalyticPrivilegeIFile);
-    projectFilesModificator.modifyProjectFiles(project, projectFiles);
-
-    assertEquals(projectFiles.get(0).getContent().length, 0);
+  public void testModifyProjectFilesSuccessfullyWhenHdbprocedure() throws IOException {
+    modifyProjectFilesAssertArrayEqualsWhenModified("testSuccessful.hdbprocedure");
   }
 
   @Test
-  public void modifyProjectFilesWhenAnalyticPrivilegeIsWrongFormat() {
-    List<IFile> projectFiles = new ArrayList<>();
-
-    byte[] analyticPrivilegeFileContent = "Some content.".getBytes();
-
-    IFile analyticPrivilegeIFile = project.createFile("test.analyticprivilege", analyticPrivilegeFileContent);
-    projectFiles.add(analyticPrivilegeIFile);
-    projectFilesModificator.modifyProjectFiles(project, projectFiles);
-
-    assertArrayEquals(projectFiles.get(0).getContent(), analyticPrivilegeFileContent);
+  public void testModifyProjectFilesSuccessfullyWhenHdbTablefunction() throws IOException {
+    modifyProjectFilesAssertArrayEqualsWhenModified("testSuccessful.hdbtablefunction");
   }
 
   @Test
-  public void modifyProjectFilesWithUnsupportedExtension() {
-    List<IFile> projectFiles = new ArrayList<>();
-
-    byte[] textFileContent = "Some content.".getBytes();
-
-    IFile textIFile = project.createFile("test.txt", textFileContent);
-    projectFiles.add(textIFile);
-    projectFilesModificator.modifyProjectFiles(project, projectFiles);
-
-    assertArrayEquals(projectFiles.get(0).getContent(), textFileContent);
+  public void testModifyProjectFilesSuccessfullyWhenAnalyticprivilege() throws IOException {
+    modifyProjectFilesAssertArrayEqualsWhenModified("testSuccessful.analyticprivilege");
   }
 
   @Test
-  public void modifyProjectFilesWithNoSessionUser() {
-    List<IFile> projectFiles = new ArrayList<>();
-
-    byte[] xsjsFileContent = "function sum(a, b) { return a + b; }".getBytes();
-
-    IFile noSessionUserIFile = project.createFile("test.xsjs", xsjsFileContent);
-    projectFiles.add(noSessionUserIFile);
-    projectFilesModificator.modifyProjectFiles(project, projectFiles);
-
-    assertArrayEquals(projectFiles.get(0).getContent(), xsjsFileContent);
+  public void testModifyProjectFilesSuccessfullyWhenHdbAnalyticprivilege() throws IOException {
+    modifyProjectFilesAssertArrayEqualsWhenModified("testSuccessful.hdbanalyticprivilege");
   }
 
   @Test
-  public void modifyProjectAnalyticPrivilegeFileWithoutWhereSql() {
-    List<IFile> projectFiles = new ArrayList<>();
-
-    String analyticPrivilegeFileContent =
-        "<Privilege:analyticPrivilege xmlns:Privilege=\"http://www.sap.com/ndb/BiModelPrivilege.ecore\" id=\"APGroup\" privilegeType=\"SQL_ANALYTIC_PRIVILEGE\" schemaVersion=\"1.1\">\n"
-            + "<descriptions defaultDescription=\"APGroup\"/>\n"
-            + "<securedModels>\n"
-            + "  <modelUri>test.tinydb::myview</modelUri>\n"
-            + "</securedModels>\n"
-            + "</Privilege:analyticPrivilege>";
-
-    IFile analyticPrivilegeIFile = project.createFile("test.analyticprivilege", analyticPrivilegeFileContent.getBytes());
-    projectFiles.add(analyticPrivilegeIFile);
-    projectFilesModificator.modifyProjectFiles(project, projectFiles);
-
-    assertArrayEquals(projectFiles.get(0).getContent(), analyticPrivilegeFileContent.getBytes());
+  public void testModifyProjectFilesWithEmptyContentWhenXsjs() throws IOException {
+    modifyProjectFilesAssertArrayEqualsWhenEmptyContent("testEmpty.xsjs");
   }
 
   @Test
-  public void modifyAnalyticPrivilegeModelUri() {
-    assertEquals("test.tinydb::myview", projectFilesModificator.processModelUri("test.tinydb::myview"));
+  public void testModifyProjectFilesWithEmptyContentWhenXsjslib() throws IOException {
+    modifyProjectFilesAssertArrayEqualsWhenEmptyContent("testEmpty.xsjslib");
+  }
+
+  @Test
+  public void testModifyProjectFilesWithEmptyContentWhenHdbprocedure() throws IOException {
+    modifyProjectFilesAssertArrayEqualsWhenEmptyContent("testEmpty.hdbprocedure");
+  }
+
+  @Test
+  public void testModifyProjectFilesWithEmptyContentWhenHdbtablefunction() throws IOException {
+    modifyProjectFilesAssertArrayEqualsWhenEmptyContent("testEmpty.hdbtablefunction");
+  }
+
+  @Test
+  public void testModifyProjectFilesWithEmptyContentWhenAnalyticprivilege() throws IOException {
+    modifyProjectFilesAssertArrayEqualsWhenEmptyContent("testEmpty.analyticprivilege");
+  }
+
+  @Test
+  public void testModifyProjectFilesWithEmptyContentWhenHdbanalyticprivilege() throws IOException {
+    modifyProjectFilesAssertArrayEqualsWhenEmptyContent("testEmpty.hdbanalyticprivilege");
+  }
+
+  @Test
+  public void testModifyProjectFilesWithUnsupportedExtension() throws IOException {
+    modifyProjectFilesAssertArrayEqualsWhenNoModification("testUnsupported.txt");
+  }
+
+  @Test
+  public void testModifyProjectFilesWithNoSessionUserWhenXsjs() throws IOException {
+    modifyProjectFilesAssertArrayEqualsWhenNoModification("testNoSessionUser.xsjs");
+  }
+
+  @Test
+  public void testModifyProjectFilesWithNoSessionUserWhenXsjslib() throws IOException {
+    modifyProjectFilesAssertArrayEqualsWhenNoModification("testNoSessionUser.xsjslib");
+  }
+
+  @Test
+  public void testModifyProjectFilesWithNoSessionUserWhenHdbprocedure() throws IOException {
+    modifyProjectFilesAssertArrayEqualsWhenNoModification("testNoSessionUser.hdbprocedure");
+  }
+
+  @Test
+  public void testModifyProjectFilesWithNoSessionUserWhenHdbtablefunction() throws IOException {
+    modifyProjectFilesAssertArrayEqualsWhenNoModification("testNoSessionUser.hdbtablefunction");
+  }
+
+  @Test
+  public void testModifyProjectFilesWithNoSessionUserWhenAnalyticprivilege() throws IOException {
+    modifyProjectFilesAssertArrayEqualsWhenModified("testNoSessionUser.analyticprivilege");
+  }
+
+  @Test
+  public void testModifyProjectFilesWithNoSessionUserWhenHdbanalyticprivilege() throws IOException {
+    modifyProjectFilesAssertArrayEqualsWhenModified("testNoSessionUser.hdbanalyticprivilege");
+  }
+
+  @Test
+  public void testModifyProjectFilesWhenAnalyticPrivilegeContentIsWrongFormat() throws IOException {
+    modifyProjectFilesAssertArrayEqualsWhenNoModification("testWrongFormat.analyticprivilege");
+  }
+
+  @Test
+  public void testModifyProjectFilesWhenAnalyticPrivilegeContentHasNoWhereSql() throws IOException {
+    modifyProjectFilesAssertArrayEqualsWhenModified("testNoWhereSql.analyticprivilege");
+  }
+
+  @Test
+  public void testModifyProjectFilesWhenAnalyticPrivilegeContentHasEmptyWhereSql() throws IOException {
+    modifyProjectFilesAssertArrayEqualsWhenModified("testEmptyWhereSql.analyticprivilege");
+  }
+
+  @Test
+  public void testModifyProjectFilesWhenAnalyticPrivilegeContentHasNoModelUri() throws IOException {
+    modifyProjectFilesAssertArrayEqualsWhenModified("testNoModelUri.analyticprivilege");
+  }
+
+  @Test
+  public void testModifyProjectFilesWhenAnalyticPrivilegeContentHasEmptyModelUri() throws IOException {
+    modifyProjectFilesAssertArrayEqualsWhenModified("testEmptyModelUri.analyticprivilege");
+  }
+
+  @Test
+  public void testModifyAnalyticPrivilegeModelUriRemoveUnnecessaryString() {
     assertEquals("myview", projectFilesModificator.processModelUri("/TEST.VIEWS/calculationviews/myview"));
-    assertEquals("myview", projectFilesModificator.processModelUri("myview"));
+  }
+
+  @Test
+  public void testModifyAnalyticPrivilegeModelUriSameString() {
+    assertEquals("test.tinydb::myview", projectFilesModificator.processModelUri("test.tinydb::myview"));
+  }
+
+  @Test
+  public void testModifyAnalyticPrivilegeModelUriEmptyString() {
     assertEquals("", projectFilesModificator.processModelUri(""));
   }
 
   @Test
-  public void modifyAnalyticPrivilegeWhereSql() {
+  public void testModifyAnalyticPrivilegeWhereSql() {
     assertEquals("SELECT \"HANAUserName\" FROM \"CALCVIEW\"",
         projectFilesModificator.processWhereSql("SELECT \"HANAUserName\" FROM \"_SYS_BIC\".\"APP.VIEWS/CALCVIEW\""));
+  }
+
+  @Test
+  public void testModifyAnalyticPrivilegeWhereSqlNoModificationRequired() {
     assertEquals("SELECT \"HANAUserName\" FROM \"CALCVIEW\"",
         projectFilesModificator.processWhereSql("SELECT \"HANAUserName\" FROM \"CALCVIEW\""));
+  }
+
+  @Test
+  public void testModifyAnalyticPrivilegeWhereSqlEmptyString() {
     assertEquals("", projectFilesModificator.processWhereSql(""));
+  }
+
+  private void modifyProjectFilesAssertArrayEqualsWhenModified(String fileName) throws IOException {
+    byte[] expectedModifiedFile = getByteArrayFromResourceName("/META-INF/modificators/expected-results/" + fileName);
+    byte[] fileContent = getByteArrayFromResourceName("/META-INF/modificators/files-to-modify/" + fileName);
+    List<IFile> projectFiles = getProjectFiles(fileName, fileContent);
+    projectFilesModificator.modifyProjectFiles(projectFiles);
+    assertArrayEquals(projectFiles.get(0).getContent(), expectedModifiedFile);
+  }
+
+  private void modifyProjectFilesAssertArrayEqualsWhenEmptyContent(String fileName) throws IOException {
+    List<IFile> projectFiles = getProjectFiles(fileName, new byte[0]);
+    projectFilesModificator.modifyProjectFiles(projectFiles);
+    assertEquals(projectFiles.get(0).getContent().length, 0);
+  }
+
+  private void modifyProjectFilesAssertArrayEqualsWhenNoModification(String fileName) throws IOException {
+    byte[] fileContent = getByteArrayFromResourceName("/META-INF/modificators/files-to-modify/" + fileName);
+    List<IFile> projectFiles = getProjectFiles(fileName, fileContent);
+    projectFilesModificator.modifyProjectFiles(projectFiles);
+    assertArrayEquals(projectFiles.get(0).getContent(), fileContent);
+  }
+
+  private List<IFile> getProjectFiles(String fileName, byte[] fileContent) {
+    List<IFile> projectFiles = new ArrayList<>();
+    IFile projectIFile = project.createFile(fileName, fileContent);
+    projectFiles.add(projectIFile);
+    return projectFiles;
+  }
+
+  private byte[] getByteArrayFromResourceName(String resourceName) throws IOException {
+    return XSKProjectFilesModificatorTest.class.getResourceAsStream(resourceName).readAllBytes();
   }
 }

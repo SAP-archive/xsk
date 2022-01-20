42604723210fafde463ab80428f8cc867924b110
Vladimir Mutafov <vlado.mutafov@gmail.com>
2022-01-20 17:04:03 +0200

    fix: do not use contains for dropdown items and iframes matching; refactor parts of the code

--- integration-tests/ui/src/test/java/com/xsk/integration/tests/migration/DirigibleConnectionProperties.java
+++ integration-tests/ui/src/test/java/com/xsk/integration/tests/migration/DirigibleConnectionProperties.java
@@ -2,7 +2,7 @@ package com.xsk.integration.tests.migration;
 
 class DirigibleConnectionProperties {
   static final String HOST = "127.0.0.1";
-  static final String PORT = "8080";
+  static final String PORT = "8484";
   static final String IDE_URI = "/services/v4/web/ide/";
   static final String AUTH = "dirigible:dirigible";
   static final String BASE_URL = "http://" + AUTH + "@" + HOST + ":" + PORT + IDE_URI;

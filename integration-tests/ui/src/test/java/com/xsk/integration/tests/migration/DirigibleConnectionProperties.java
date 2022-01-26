3cf4b1a1a837a159f21d026a8ba579df03ee6337
Vladimir Mutafov <vlado.mutafov@gmail.com>
2022-01-26 12:50:29 +0200

    fix: integration tests failing due to login changes in xsk (#1084)
    * fix: integration tests failing due to login changes in xsk
    
    * fix: move user and pass into constants
    
    * fix: revert headless config

--- integration-tests/ui/src/test/java/com/xsk/integration/tests/migration/DirigibleConnectionProperties.java
+++ integration-tests/ui/src/test/java/com/xsk/integration/tests/migration/DirigibleConnectionProperties.java
@@ -1,9 +1,12 @@
 package com.xsk.integration.tests.migration;
 
 class DirigibleConnectionProperties {
+
   static final String HOST = "127.0.0.1";
   static final String PORT = "8080";
   static final String IDE_URI = "/services/v4/web/ide/";
-  static final String AUTH = "dirigible:dirigible";
+  static final String AUTH_USERNAME = "dirigible";
+  static final String AUTH_PASSWORD = "dirigible";
+  static final String AUTH = AUTH_USERNAME + ":" + AUTH_PASSWORD;
   static final String BASE_URL = "http://" + AUTH + "@" + HOST + ":" + PORT + IDE_URI;
 }

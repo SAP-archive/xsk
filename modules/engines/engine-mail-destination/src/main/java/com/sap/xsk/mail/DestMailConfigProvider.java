248a345de0ee202fd2fb91fea61ea4501d46c43e
Boris Nenchovski <bobinen@gmail.com>
2022-02-01 17:30:10 +0200

    DestMailConfigProvider now works with Destinations
    Signed-off-by: Boris Nenchovski <bobinen@gmail.com>

--- modules/engines/engine-mail-destination/src/main/java/com/sap/xsk/mail/DestMailConfigProvider.java
+++ modules/engines/engine-mail-destination/src/main/java/com/sap/xsk/mail/DestMailConfigProvider.java
@@ -11,12 +11,32 @@
  */
 package com.sap.xsk.mail;
 
+import com.sap.cloud.sdk.cloudplatform.connectivity.exception.DestinationAccessException;
+import com.sap.xsk.api.destination.CloudPlatformDestinationFacade;
 import org.eclipse.dirigible.api.v3.mail.api.IMailConfigurationProvider;
+import com.sap.cloud.sdk.cloudplatform.connectivity.Destination;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.List;
 import java.util.Properties;
 
 public class DestMailConfigProvider implements IMailConfigurationProvider {
+  private static final String MAIL_USER = "mail.user";
+  private static final String MAIL_PASSWORD = "mail.password";
+  private static final String MAIL_TRANSPORT_PROTOCOL = "mail.transport.protocol";
+  private static final String MAIL_SMTPS_HOST = "mail.smtps.host";
+  private static final String MAIL_SMTPS_PORT = "mail.smtps.port";
+  private static final String MAIL_SMTPS_AUTH = "mail.smtps.auth";
+  private static final String MAIL_SMTP_HOST = "mail.smtp.host";
+  private static final String MAIL_SMTP_PORT = "mail.smtp.port";
+  private static final String MAIL_SMTP_AUTH = "mail.smtp.auth";
+  private static List<String> MAIL_PROPERTIES = List.of(MAIL_USER, MAIL_PASSWORD, MAIL_TRANSPORT_PROTOCOL, MAIL_SMTPS_HOST, MAIL_SMTPS_PORT,
+      MAIL_SMTPS_AUTH, MAIL_SMTP_HOST, MAIL_SMTP_PORT, MAIL_SMTP_AUTH);
 
   private static final String PROVIDER_NAME = "destination";
+  private static final String DESTINATION_NAME = "Mail";
+
+  private static final Logger logger = LoggerFactory.getLogger(DestMailConfigProvider.class);
 
   @Override
   public String getName() {
@@ -26,7 +46,20 @@ public class DestMailConfigProvider implements IMailConfigurationProvider {
   @Override
   public Properties getProperties() {
     Properties properties = new Properties();
-    properties.put("PLACEHOLDER", "PLACEHOLDER");
+    try {
+      Destination destination = CloudPlatformDestinationFacade.getDestination(DESTINATION_NAME);
+      for (String key : MAIL_PROPERTIES) {
+        if(!destination.get(key).isEmpty()) {
+          properties.put(key, destination.get(key).get());
+        }
+      }
+    } catch (DestinationAccessException e) {
+      logger.error("No Destination object with name " + DESTINATION_NAME + " found!");
+    }
+
+    if(properties.isEmpty()) {
+      logger.error("Destination object does not contain necessary mail settings!");
+    }
     return properties;
   }
 }

c0678d816e0f2faff4768732a72b4cb72de9e3fa
ThuF <jordan.p.pavlov@gmail.com>
2022-03-24 10:06:51 +0200

    Updates Eclipse Dirigible version

--- modules/engines/engine-xsjs/src/test/resources/META-INF/dirigible/test/xsk/error_polyfills/throwRegularNestedLevelError.js
+++ modules/engines/engine-xsjs/src/test/resources/META-INF/dirigible/test/xsk/error_polyfills/throwRegularNestedLevelError.js
@@ -6,6 +6,5 @@ try {
   }
   someNestedFunction();
 } catch (e) {
-//  Assert.assertEquals("Unexpected exception fileName", "/throwRegularNestedLevelError.js", e.fileName);
-  Assert.assertEquals("Unexpected exception fileName", "Unknown", e.fileName); // replace with line above once Dirigible 6.2.13 is released
+  Assert.assertEquals("Unexpected exception fileName", "/throwRegularNestedLevelError.js", e.fileName);
 }
--- modules/engines/engine-xsjs/src/test/resources/META-INF/dirigible/test/xsk/error_polyfills/throwRegularRootLevelError.js
+++ modules/engines/engine-xsjs/src/test/resources/META-INF/dirigible/test/xsk/error_polyfills/throwRegularRootLevelError.js
@@ -3,6 +3,5 @@ const Assert = Java.type("org.junit.Assert");
 try {
   throw new Error("some error");
 } catch (e) {
-//  Assert.assertEquals("Unexpected exception fileName", "/throwRegularRootLevelError.js", e.fileName);
-  Assert.assertEquals("Unexpected exception fileName", "Unknown", e.fileName); // replace with line above once Dirigible 6.2.13 is released
+  Assert.assertEquals("Unexpected exception fileName", "/throwRegularRootLevelError.js", e.fileName);
 }

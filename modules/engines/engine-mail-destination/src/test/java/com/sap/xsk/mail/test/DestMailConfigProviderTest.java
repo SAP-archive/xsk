6e8c15e32c49c400685d8f4af55465d77cbdd918
Dermendzhiev, Teodor (external - Project) <tdermendjievft@gmail.com>
2022-04-06 09:09:58 +0300

    Add test for DestMailConfigProvider

---
+++ modules/engines/engine-mail-destination/src/test/java/com/sap/xsk/mail/test/DestMailConfigProviderTest.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2022 SAP SE or an SAP affiliate company and XSK contributors
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License, v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * SPDX-FileCopyrightText: 2022 SAP SE or an SAP affiliate company and XSK contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package com.sap.xsk.mail.test;
+
+import com.sap.xsk.api.destination.CloudPlatformDestinationFacade;
+import com.sap.xsk.mail.DestMailConfigProvider;
+import org.eclipse.dirigible.commons.config.Configuration;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import java.util.Properties;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+public class DestMailConfigProviderTest {
+
+  private static final String MAIL_SERVER_DESTINATION_NAME = "MAIL_SERVER_DESTINATION_NAME";
+  private static final String DESTINATION_NAME = "test-destination";
+
+  MockedStatic<CloudPlatformDestinationFacade> cloudPlatformDestinationFacadeMock;
+  MockedStatic<Configuration>configurationMock;
+
+  @Before
+  public void setup() {
+    mock();
+  }
+
+  @After
+  public void close() {
+    configurationMock.close();
+    cloudPlatformDestinationFacadeMock.close();
+  }
+
+  public void mock() {
+    Properties props = new Properties();
+    props.setProperty("mail.user", "dirigible@eclipse.com");
+
+    configurationMock = Mockito.mockStatic(Configuration.class);
+    configurationMock.when(() -> Configuration.get(MAIL_SERVER_DESTINATION_NAME))
+        .thenReturn(DESTINATION_NAME);
+    com.sap.xsk.api.destination.Destination mockedDestination = Mockito.mock(com.sap.xsk.api.destination.Destination.class);
+    when(mockedDestination.getProperties()).thenReturn(props);
+
+    cloudPlatformDestinationFacadeMock = Mockito.mockStatic(CloudPlatformDestinationFacade.class);
+    cloudPlatformDestinationFacadeMock.when(() -> CloudPlatformDestinationFacade.getDestination(DESTINATION_NAME))
+        .thenReturn(mockedDestination);
+  }
+
+  @Test
+  public void getPropertiesTest() {
+    DestMailConfigProvider provider = new DestMailConfigProvider();
+    Properties properties = provider.getProperties();
+    assertEquals(1, properties.size());
+  }
+
+}

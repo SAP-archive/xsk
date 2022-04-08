25119a9f9acb3ff7aee7dc58615faf6c052ba767
aihanrashidov <aihan_rashidov@mail.bg>
2022-04-08 16:02:34 +0300

    Removed unused mock

--- modules/engines/engine-xsjs/src/test/java/com/sap/xsk/engine/api/test/XSKApiSuiteTest.java
+++ modules/engines/engine-xsjs/src/test/java/com/sap/xsk/engine/api/test/XSKApiSuiteTest.java
@@ -167,7 +167,6 @@ public class XSKApiSuiteTest extends AbstractDirigibleTest {
   }
 
   private void mockResponse(HttpServletResponse mockedResponse) throws IOException {
-    when(mockedResponse.getHeaderNames()).thenReturn(Arrays.asList("TestHeader1", "TestHeader2"));
     when(mockedResponse.getOutputStream()).thenReturn(new StubServletOutputStream());
   }
 

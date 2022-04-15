274ee06f9cb02d02fd3aa3e10433f4419bd9ea10
Vladimir Mutafov <vlado.mutafov@gmail.com>
2022-04-15 15:46:21 +0300

    chore: split up code

--- modules/engines/engine-xsodata/src/test/java/com/sap/xsk/xsodata/listener/XSODataInitializerTest.java
+++ modules/engines/engine-xsodata/src/test/java/com/sap/xsk/xsodata/listener/XSODataInitializerTest.java
@@ -11,27 +11,63 @@
  */
 package com.sap.xsk.xsodata.listener;
 
+import org.apache.commons.lang3.reflect.FieldUtils;
 import org.apache.olingo.odata2.api.edm.EdmException;
-import org.apache.olingo.odata2.core.edm.provider.EdmImplProv;
 import org.apache.olingo.odata2.core.edm.provider.EdmNamedImplProv;
+import org.hamcrest.MatcherAssert;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.doThrow;
 
 public class XSODataInitializerTest {
 
+  private XSODataInitializer xsoDataInitializer;
+
+  @Before
+  public void setUp() {
+    xsoDataInitializer = new XSODataInitializer();
+  }
+
   @Test
   public void testWhenValidationPatternIsPatchedNamesCanHaveDot() throws EdmException {
-    new XSODataInitializer().contextInitialized(null);
+    xsoDataInitializer.contextInitialized(null);
 
-    String expectedName = "test.name";
-    DummyEdmNamedImplProv dummyEdmNamedImplProv = new DummyEdmNamedImplProv(expectedName);
+    var expectedName = "test.name";
+    var dummyEdmNamedImplProv = new DummyEdmNamedImplProv(expectedName);
+
     assertEquals("Unexpected name", "test.name", dummyEdmNamedImplProv.getName());
   }
 
   @Test
   public void testWhenValidationPatternIsNotPatchedNamesCanNotHaveDot() throws EdmException {
     assertThrows(EdmException.class, () -> new DummyEdmNamedImplProv("test.name"));
+  }
+
+  @Test
+  public void testWhenValidationPatcherThrowsExceptionNewIllegalStateExceptionIsThrown() throws ReflectiveOperationException {
+    var odataInternalValidationPatternPatcherMock = Mockito.mock(ODataNamesValidationPatternPatcher.class);
+    doThrow(ReflectiveOperationException.class).when(odataInternalValidationPatternPatcherMock).patch();
+    FieldUtils.writeField(xsoDataInitializer, "odataNamesValidationPatternPatcher",odataInternalValidationPatternPatcherMock,true);
+
+    var illegalStateException = assertThrows(
+        IllegalStateException.class,
+        () -> xsoDataInitializer.contextInitialized(null)
+    );
+
+    assertEquals(
+        "Unexpected exception message",
+        "Failed to replace default Olingo OData parameter name pattern.",
+        illegalStateException.getMessage()
+    );
+    MatcherAssert.assertThat(
+        "Unexpected exception cause",
+        illegalStateException.getCause(),
+        instanceOf(ReflectiveOperationException.class)
+    );
   }
 
   class DummyEdmNamedImplProv extends EdmNamedImplProv {

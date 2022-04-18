adec32fb77aebb356458e7489ae198a726986ee5
Fluctuationqt <outrageousxqt@gmail.com>
2022-04-18 11:58:42 +0300

    chore: tests use h2

--- modules/engines/engine-xsjs/src/test/resources/META-INF/dirigible/test/xsk/exports/exportsGeneratorTest.mjs
+++ modules/engines/engine-xsjs/src/test/resources/META-INF/dirigible/test/xsk/exports/exportsGeneratorTest.mjs
@@ -7,9 +7,9 @@ import { digest } from '@dirigible-v4/utils'
 
 let stateTableParams = {
   name: "XSJSLIB_EXPORT_TEST_TABLE",
-  schema: "APP",
+  schema: "PUBLIC",
   location: "local",
-  db: "DefaultDB"
+  db: "SystemDB"
 }
 
 function _fetchAllEntriesInTable() {

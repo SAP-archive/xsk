ef864a110fdf2db958bf00b4dc900b9ffb6b7210
Ivan <iwolkkov@gmail.com>
2022-05-04 09:32:09 +0300

    added tests for string input w/o key

--- modules/engines/engine-xssecurity/src/test/java/XSKSecureStoreCryptoFacadeTest.java
+++ modules/engines/engine-xssecurity/src/test/java/XSKSecureStoreCryptoFacadeTest.java
@@ -15,6 +15,8 @@ public class XSKSecureStoreCryptoFacadeTest {
     // string value of "value" as bytes
     byte[]input  = {118, 97, 108, 117, 101};
 
+    // tests for input as byte[]
+
     @Test
     public void getMD5() throws NoSuchAlgorithmException, InvalidKeyException {
         var hash = XSKSecureCryptoFacade.md5(input,null);
@@ -48,6 +50,45 @@ public class XSKSecureStoreCryptoFacadeTest {
     @Test
     public void getSHA256withKey() throws NoSuchAlgorithmException, InvalidKeyException {
         var hash = XSKSecureCryptoFacade.sha256(input,"key");
+        byte [] expected = {-112, -5, -4, -15, 94, 116, -93, 107, -119, -37, -37, 42, 114, 29, -102, -20, -1, -33, -35, -36, 92, -125, -30, 127, 117, -110, 89, 79, 113, -109, 36, -127};
+        assertArrayEquals(hash,expected);
+    }
+
+    //test with input as string
+
+    @Test
+    public void getMD5FromString() throws NoSuchAlgorithmException, InvalidKeyException {
+        var hash = XSKSecureCryptoFacade.md5("value",null);
+        byte [] expected = {32, 99, -63, 96, -115, 110, 11, -81, -128, 36, -100, 66, -30, -66, 88, 4};
+        assertArrayEquals(hash,expected);
+    }
+    @Test
+    public void getMD5withKeyFromString() throws NoSuchAlgorithmException, InvalidKeyException {
+        var hash = XSKSecureCryptoFacade.md5("value","key");
+        byte [] expected = {1, 67, 62, -3, 95, 22, 50, 126, -92, -77, 17, 68, 87, 44, 103, -10};
+        assertArrayEquals(hash,expected);
+    }
+    @Test
+    public void getSHA1FromString() throws NoSuchAlgorithmException, InvalidKeyException {
+        var hash = XSKSecureCryptoFacade.sha1("value",null);
+        byte [] expected = {-13, 43, 103, -57, -30, 99, 66, -81, 66, -17, -85, -58, 116, -44, 65, -36, -96, -94, -127, -59};
+        assertArrayEquals(hash,expected);
+    }
+    @Test
+    public void getSHA1withKeyFromString() throws NoSuchAlgorithmException, InvalidKeyException {
+        var hash = XSKSecureCryptoFacade.sha1("value","key");
+        byte [] expected = {87, 68, 58, 76, 5, 35, 80, -92, 70, 56, -125, 93, 100, -3, 102, -126, 47, -127, 51, 25};
+        assertArrayEquals(hash,expected);
+    }
+    @Test
+    public void getSHA256FromString() throws NoSuchAlgorithmException, InvalidKeyException {
+        var hash = XSKSecureCryptoFacade.sha256("value",null);
+        byte [] expected = {-51, 66, 64, 77, 82, -83, 85, -52, -6, -102, -54, 74, -36, -126, -118, -91, -128, 10, -39, -45, -123, -96, 103, 31, -68, -65, 114, 65, 24, 50, 6, 25};
+        assertArrayEquals(hash,expected);
+    }
+    @Test
+    public void getSHA256withKeyFromString() throws NoSuchAlgorithmException, InvalidKeyException {
+        var hash = XSKSecureCryptoFacade.sha256("value","key");
         byte [] expected = {-112, -5, -4, -15, 94, 116, -93, 107, -119, -37, -37, 42, 114, 29, -102, -20, -1, -33, -35, -36, 92, -125, -30, 127, 117, -110, 89, 79, 113, -109, 36, -127};
         assertArrayEquals(hash,expected);
     }

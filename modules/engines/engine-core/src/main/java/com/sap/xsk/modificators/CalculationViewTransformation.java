e5afe7e7a7c4f2ef4708d5c40ce6957ff62368f6
dimitarquanterall <d.aleksandrov@sap.com>
2022-05-19 15:13:34 +0300

    Requested class rename
    Signed-off-by: dimitarquanterall <d.aleksandrov@sap.com>

--- modules/engines/engine-core/src/main/java/com/sap/xsk/modificators/CalculationViewTransformation.java
+++ modules/engines/engine-core/src/main/java/com/sap/xsk/modificators/CalculationViewTransformation.java
@@ -21,7 +21,7 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.StringReader;
 
-public class CalculationViewDataSourceTransformation {
+public class CalculationViewTransformation {
 
   private static final String CALCULATION_VIEW_DATA_SOURCE_TRANSFORMATION_XSLT = "<xsl:stylesheet version=\"1.0\" xmlns:xsl=\"http://www.w3.org/1999/XSL/Transform\">\n"
       + "    <xsl:template match=\"DataSource/@type\" />\n"

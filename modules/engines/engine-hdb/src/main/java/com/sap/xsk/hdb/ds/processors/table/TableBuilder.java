86f3a8cd6f31f3cab0382f2c5986974fccee542c
Boris Nenchovski <bobinen@gmail.com>
2022-06-17 15:02:21 +0300

    XSKTableAlterProcessor now builds alter statements for calculated columns
    Signed-off-by: Boris Nenchovski <bobinen@gmail.com>

--- modules/engines/engine-hdb/src/main/java/com/sap/xsk/hdb/ds/processors/table/TableBuilder.java
+++ modules/engines/engine-hdb/src/main/java/com/sap/xsk/hdb/ds/processors/table/TableBuilder.java
@@ -11,6 +11,7 @@
  */
 package com.sap.xsk.hdb.ds.processors.table;
 
+import com.sap.xsk.hdb.ds.model.hdbtable.XSKDataStructureHDBTableCalculatedColumnModel;
 import com.sap.xsk.hdb.ds.model.hdbtable.XSKDataStructureHDBTableColumnModel;
 import com.sap.xsk.hdb.ds.model.hdbtable.XSKDataStructureHDBTableConstraintCheckModel;
 import com.sap.xsk.hdb.ds.model.hdbtable.XSKDataStructureHDBTableConstraintForeignKeyModel;
@@ -28,6 +29,7 @@ import org.eclipse.dirigible.database.sql.ISqlKeywords;
 import org.eclipse.dirigible.database.sql.SqlFactory;
 import org.eclipse.dirigible.database.sql.Table;
 import org.eclipse.dirigible.database.sql.builders.table.AbstractTableBuilder;
+import org.eclipse.dirigible.database.sql.dialects.hana.HanaAlterTableBuilder;
 import org.eclipse.dirigible.database.sql.dialects.hana.HanaCreateTableBuilder;
 import org.eclipse.dirigible.database.sql.dialects.hana.HanaSqlDialect;
 
@@ -48,6 +50,16 @@ public class TableBuilder {
     return sqlTableBuilder.buildTable();
   }
 
+  public String buildAlterCalculatedColumns(String tableName, XSKDataStructureHDBTableCalculatedColumnModel calculatedColumnModel) {
+
+    HanaAlterTableBuilder sqlTableBuilder = alterTableBuilder(tableName);
+    String calculatedColumnStatement = buildCalculatedColumnStatement(calculatedColumnModel);
+    sqlTableBuilder.add().column(calculatedColumnModel.getColumnName(), calculatedColumnModel.getType(), false, true,
+          false, calculatedColumnStatement);
+
+    return sqlTableBuilder.build();
+  }
+
   private HanaCreateTableBuilder createTableBuilder(String tableName, String tableType) {
     HanaSqlDialect dialect = new HanaSqlDialect();
     if (null != tableType) {
@@ -60,6 +72,12 @@ public class TableBuilder {
     }
 
     return SqlFactory.getNative(dialect).create().table(tableName);
+  }
+
+  private HanaAlterTableBuilder alterTableBuilder(String tableName) {
+    HanaSqlDialect dialect = new HanaSqlDialect();
+
+    return SqlFactory.getNative(dialect).alter().table(tableName);
   }
 
   private void addTableIndicesToBuilder(HanaCreateTableBuilder sqlTableBuilder, XSKDataStructureHDBTableModel tableModel) {
@@ -196,6 +214,12 @@ public class TableBuilder {
         builder.unique(uniqueIndexName, uniqueIndexColumns, indexType, indexOrder);
       }
     }
+  }
+
+  private String buildCalculatedColumnStatement(XSKDataStructureHDBTableCalculatedColumnModel calculatedColumnModel) {
+    StringBuilder statement = new StringBuilder();
+    statement.append("(").append(calculatedColumnModel.getLength()).append(") AS ").append(calculatedColumnModel.getStatement());
+    return statement.toString();
   }
 }
 
--- modules/engines/engine-hdb/src/main/java/com/sap/xsk/hdb/ds/processors/table/XSKTableAlterProcessor.java
+++ modules/engines/engine-hdb/src/main/java/com/sap/xsk/hdb/ds/processors/table/XSKTableAlterProcessor.java
@@ -11,12 +11,23 @@
  */
 package com.sap.xsk.hdb.ds.processors.table;
 
+import com.sap.xsk.hdb.ds.artefacts.HDBTableSynchronizationArtefactType;
+import com.sap.xsk.hdb.ds.model.hdbtable.XSKDataStructureHDBTableCalculatedColumnModel;
 import com.sap.xsk.hdb.ds.model.hdbtable.XSKDataStructureHDBTableModel;
 import com.sap.xsk.hdb.ds.processors.AbstractXSKProcessor;
+import com.sap.xsk.utils.XSKCommonsConstants;
+import com.sap.xsk.utils.XSKCommonsUtils;
+import com.sap.xsk.utils.XSKHDBUtils;
+import org.eclipse.dirigible.core.scheduler.api.ISynchronizerArtefactType.ArtefactState;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class XSKTableAlterProcessor extends AbstractXSKProcessor<XSKDataStructureHDBTableModel> {
+
+  private static final Logger logger = LoggerFactory.getLogger(XSKTableAlterProcessor.class);
+  private static final HDBTableSynchronizationArtefactType TABLE_ARTEFACT = new HDBTableSynchronizationArtefactType();
 
   /**
    * Execute the corresponding statement.
@@ -27,17 +38,45 @@ public class XSKTableAlterProcessor extends AbstractXSKProcessor<XSKDataStructur
    */
   @Override
   public boolean execute(Connection connection, XSKDataStructureHDBTableModel tableModel) throws SQLException {
+
+    for(XSKDataStructureHDBTableCalculatedColumnModel columnModel: tableModel.getCalculatedColumns()) {
+      String tableName = XSKHDBUtils.escapeArtifactName(tableModel.getName(), tableModel.getSchema());
+      String tableAlterStatement = new TableBuilder().buildAlterCalculatedColumns(tableName, columnModel);
+      boolean success = processStatement(connection, tableModel, tableAlterStatement);
+      if(!success) {
+        return false;
+      }
+    }
+
     XSKTableAlterHandler handler = createTableAlterHandler(connection, tableModel);
     handler.addColumns(connection);
     handler.removeColumns(connection);
     handler.updateColumns(connection);
     handler.rebuildIndeces(connection);
     handler.ensurePrimaryKeyIsUnchanged(connection);
+
     return true;
   }
 
   public XSKTableAlterHandler createTableAlterHandler(Connection connection, XSKDataStructureHDBTableModel tableModel)
       throws SQLException {
     return new XSKTableAlterHandler(connection, tableModel);
+  }
+
+  private boolean processStatement(Connection connection, XSKDataStructureHDBTableModel tableModel, String tableCreateStatement) {
+    try {
+      executeSql(tableCreateStatement, connection);
+
+      String message = String.format("Altered table %s successfully", tableModel.getName());
+      applyArtefactState(tableModel.getName(), tableModel.getLocation(), TABLE_ARTEFACT, ArtefactState.SUCCESSFUL_UPDATE, message);
+      return true;
+    } catch (SQLException ex) {
+      logger.error("Alter of table failed. Used SQL - alter table {}", tableCreateStatement, ex);
+      XSKCommonsUtils.logProcessorErrors(ex.getMessage(), XSKCommonsConstants.PROCESSOR_ERROR, tableModel.getLocation(),
+          XSKCommonsConstants.HDB_TABLE_PARSER);
+      String message = String.format("Alter table [%s] failed due to an error: %s", tableModel, ex.getMessage());
+      applyArtefactState(tableModel.getName(), tableModel.getLocation(), TABLE_ARTEFACT, ArtefactState.FAILED_UPDATE, message);
+      return false;
+    }
   }
 }
